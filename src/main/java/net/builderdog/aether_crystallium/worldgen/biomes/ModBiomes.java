b184f65c2448f4b413cd3a4c1e14c97fafab7c1a
Builderdog841 <timon.kempter@gmail.com>
2023-06-16 19:27:16 +0200

    Failed attempt to make Frostpines generate

---
+++ src/main/java/net/builderdog/aether_crystallium/worldgen/biomes/ModBiomes.java
@@ -0,0 +1,16 @@
+package net.builderdog.aether_crystallium.worldgen.biomes;
+
+import net.builderdog.aether_crystallium.AetherCrystallium;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biome;
+
+public class ModBiomes {
+    public static final ResourceKey<Biome> FROSTPINES = createKey("frostpines");
+    public static final ResourceKey<Biome> QUICKSOIL_DESERT = createKey("quicksoil_desert");
+
+    private static ResourceKey<Biome> createKey(String name) {
+        return ResourceKey.create(Registries.BIOME, new ResourceLocation(AetherCrystallium.MOD_ID, name));
+    }
+}
