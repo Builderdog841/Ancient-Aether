1620e93ba4e5328ad0d9ea59645d4c475883dc96
Builderdog841 <timon.kempter@gmail.com>
2023-07-02 13:59:38 +0200

    Improve Biome Gen and Update Highlands Pine Sapling Ttexture

--- src/main/java/net/builderdog/aether_crystallium/world/biomes/ModRegion.java
+++ src/main/java/net/builderdog/aether_crystallium/world/biomes/ModRegion.java
@@ -24,28 +24,28 @@ public class ModRegion extends Region {
         Climate.Parameter fullRange = Climate.Parameter.span(-1.0F, 1.0F);
 
         //ENCHANTED WOODS
-        Climate.Parameter temps1 = Climate.Parameter.span(0.7F, 0.93F);
+        Climate.Parameter temps1 = Climate.Parameter.span(0F, 0.3F);
 
         //QUICKSOIL DESERT
-        Climate.Parameter temps2 = Climate.Parameter.span(-1.0F, -0.65F);
+        Climate.Parameter temps2 = Climate.Parameter.span(0.5F, 1F);
 
         //HIGHLANDS TAIGA
-        Climate.Parameter temps3 = Climate.Parameter.span(0F, 0.4F);
+        Climate.Parameter temps3 = Climate.Parameter.span(-1.0F, -0.5F);
 
         //HIGHFIELDS
-        Climate.Parameter temps4 = Climate.Parameter.span(0.4F, 0.93F);
+        Climate.Parameter temps4 = Climate.Parameter.span(-0.3F, 0F);
 
 
-        this.addBiome(mapper, new Climate.ParameterPoint(temps1, Climate.Parameter.span(0.0F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(temps1, Climate.Parameter.span(0.0F, 0.4F), fullRange, fullRange, fullRange, fullRange, 0),
                 ModBiomes.ENCHANTED_WOODS);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(temps2, Climate.Parameter.span(-1.0F, 0.0F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(temps2, Climate.Parameter.span(0.5F, 1F), fullRange, fullRange, fullRange, fullRange, 0),
                 ModBiomes.QUICKSOIL_DESERT);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(temps3, Climate.Parameter.span(-1.0F, 0.3F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(temps3, Climate.Parameter.span(-1.0F, -0.3F), fullRange, fullRange, fullRange, fullRange, 0),
                 ModBiomes.HIGHLANDS_TAIGA);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(temps4, Climate.Parameter.span(0.3F, 0.8F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(temps4, Climate.Parameter.span(-0.3F, 0F), fullRange, fullRange, fullRange, fullRange, 0),
                 ModBiomes.HIGHFIELDS);
     }
 }
