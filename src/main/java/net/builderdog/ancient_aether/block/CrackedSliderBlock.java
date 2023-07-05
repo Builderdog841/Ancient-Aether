00b257cc4e3054d64e91cd97045fc38f7bab4549
Builderdog841 <timon.kempter@gmail.com>
2023-07-05 10:32:07 +0200

    Cracked Slider Rotation

--- src/main/java/net/builderdog/ancient_aether/block/CrackedSliderBlock.java
+++ src/main/java/net/builderdog/ancient_aether/block/CrackedSliderBlock.java
@@ -1,21 +1,34 @@
 package net.builderdog.ancient_aether.block;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RedstoneLampBlock;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
 public class CrackedSliderBlock extends RedstoneLampBlock {
-    public CrackedSliderBlock(Properties properties) {
-        super(properties);
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final BooleanProperty LIT = RedstoneTorchBlock.LIT;
+    protected CrackedSliderBlock(BlockBehaviour.Properties p_48687_) {
+        super(p_48687_);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(LIT, Boolean.valueOf(false)));
     }
-
-    @Override
-    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return box(0, 0, 0, 32, 32, 32);
+    public BlockState getStateForPlacement(BlockPlaceContext p_48689_) {
+        return this.defaultBlockState().setValue(FACING, p_48689_.getHorizontalDirection().getOpposite());
     }
-
+    public RenderShape getRenderShape(BlockState p_48727_) {
+        return RenderShape.MODEL;
+    }
+    public BlockState rotate(BlockState p_48722_, Rotation p_48723_) {
+        return p_48722_.setValue(FACING, p_48723_.rotate(p_48722_.getValue(FACING)));
+    }
+    public BlockState mirror(BlockState p_48719_, Mirror p_48720_) {
+        return p_48719_.rotate(p_48720_.getRotation(p_48719_.getValue(FACING)));
+    }
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_48725_) {
+        p_48725_.add(FACING, LIT);
+    }
 }
