3aaa96750f1e50a56e4a78a47fdfbaf28e45ba74
Builderdog841 <timon.kempter@gmail.com>
2023-07-21 20:24:25 +0200

    Enchanted Runes now unlock the Library and Tag Fixes

--- src/main/java/net/builderdog/ancient_aether/item/EnchantedSentryRuneItem.java
+++ src/main/java/net/builderdog/ancient_aether/item/EnchantedSentryRuneItem.java
@@ -13,6 +13,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -22,8 +23,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.Vec3;
 
 public class EnchantedSentryRuneItem extends Item {
     public EnchantedSentryRuneItem(Properties properties) {
@@ -37,6 +36,10 @@ public class EnchantedSentryRuneItem extends Item {
         ItemStack item = context.getItemInHand();
         BlockState state = level.getBlockState(pos);
         Player player = context.getPlayer();
+        LevelAccessor world = context.getLevel();
+        double x = context.getClickedPos().getX();
+        double y = context.getClickedPos().getY();
+        double z = context.getClickedPos().getZ();
 
         if (state.getBlock() == AncientAetherBlocks.BROKEN_ENCHANTED_OBELISK.get()) {
             if (player instanceof ServerPlayer _plr0 && _plr0.level instanceof ServerLevel
@@ -45,19 +48,28 @@ public class EnchantedSentryRuneItem extends Item {
                     level.setBlockAndUpdate(pos, AncientAetherBlocks.ENCHANTED_OBELISK.get().defaultBlockState());
                     player.awardStat(Stats.ITEM_USED.get(item.getItem()));
                     item.shrink(1);
-                    if (state.getBlock() == AetherBlocks.LOCKED_CARVED_STONE.get()) {
-                        level.setBlockAndUpdate(pos, AetherBlocks.CARVED_STONE.get().defaultBlockState());
-                    }
-                    if (state.getBlock() == AncientAetherBlocks.LOCKED_ENCHANTED_SENTRY_STONE.get()) {
-                        level.setBlockAndUpdate(pos, AncientAetherBlocks.ENCHANTED_SENTRY_STONE.get().defaultBlockState());
-                    }
-                    if (state.getBlock() == AncientAetherBlocks.LOCKED_CARVED_STONE_MOSAIC.get()) {
-                                level.setBlockAndUpdate(pos, AncientAetherBlocks.CARVED_STONE_MOSAIC.get().defaultBlockState());
-                    }
-                    if (state.getBlock() == AetherBlocks.BOSS_DOORWAY_CARVED_STONE.get()) {
-                        level.setBlockAndUpdate(pos, Blocks.AIR.defaultBlockState());
+                    for (int x1 = Mth.floor(x - 30); x1 <= Mth.ceil(x + 30); x1++) {
+                        for (int y1 = Mth.floor(y - 20); y1 <= Mth.ceil(y + 20); y1++) {
+                            for (int z1 = Mth.floor(z - 30); z1 <= Mth.ceil(z + 30); z1++) {
+                                BlockPos checkedPos = new BlockPos(x1, y1, z1);
+                                BlockState checkedState = level.getBlockState(checkedPos);
+
+                                if (checkedState.getBlock() == AetherBlocks.LOCKED_CARVED_STONE.get()) {
+                                    level.setBlockAndUpdate(checkedPos, AetherBlocks.CARVED_STONE.get().defaultBlockState());
+                                }
+                                if (checkedState.getBlock() == AncientAetherBlocks.LOCKED_CARVED_STONE_MOSAIC.get()) {
+                                    level.setBlockAndUpdate(checkedPos, AncientAetherBlocks.CARVED_STONE_MOSAIC.get().defaultBlockState());
+                                }
+                                if (checkedState.getBlock() == AncientAetherBlocks.LOCKED_ENCHANTED_SENTRY_STONE.get()) {
+                                    level.setBlockAndUpdate(checkedPos, AncientAetherBlocks.ENCHANTED_SENTRY_STONE.get().defaultBlockState());
+                                }
+                                if (checkedState.getBlock() == AetherBlocks.BOSS_DOORWAY_CARVED_STONE.get()) {
+                                    level.setBlockAndUpdate(checkedPos, Blocks.AIR.defaultBlockState());
                                 }
                             }
+                        }
+                    }
+                }
                         } else {
                             if (player != null && !level.isClientSide) {
                                 player.displayClientMessage(Component.translatable("gui.ancient_aether.obelisk_valkyrie_queen").withStyle(ChatFormatting.RED), true);
