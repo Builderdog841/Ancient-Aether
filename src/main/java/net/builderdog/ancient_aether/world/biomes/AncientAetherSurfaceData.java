09ad3eee4b2b86909d145c7554b47ad3b4346d62
Builderdog841 <timon.kempter@gmail.com>
2023-08-03 12:04:03 +0200

    Tweaked some more values

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherSurfaceData.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherSurfaceData.java
@@ -2,6 +2,7 @@ package net.builderdog.ancient_aether.world.biomes;
 
 import com.aetherteam.aether.block.AetherBlockStateProperties;
 import com.aetherteam.aether.block.AetherBlocks;
+import com.aetherteam.aether.data.resources.registries.AetherBiomes;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraft.world.level.levelgen.placement.CaveSurface;
 
@@ -11,11 +12,10 @@ public class AncientAetherSurfaceData {
     {
         return SurfaceRules.sequence(
 
-                SurfaceRules.ifTrue(SurfaceRules.isBiome(net.builderdog.ancient_aether.world.biomes.AncientAetherBiomes.QUICKSOIL_DESERT),
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AncientAetherBiomes.QUICKSOIL_DESERT),
                         SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(AetherBlocks.QUICKSOIL.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true)))),
-
                 SurfaceRules.ifTrue(SurfaceRules.isBiome(AncientAetherBiomes.QUICKSOIL_DESERT),
                 SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(0, true, 0, CaveSurface.FLOOR), SurfaceRules.state(AetherBlocks.QUICKSOIL.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, true))))
-    );
+        );
     }
 }
