1a63e4998115fef096b18efd3d67aeae39f45c81
Builderdog841 <timon.kempter@gmail.com>
2023-08-07 21:55:24 +0200

    Quicksoil Desert Generation

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
@@ -27,7 +27,7 @@ public class AncientAetherRegion extends Region {
 
         Climate.Parameter tempDefault1 = Climate.Parameter.span(-1.0F, -0.8F);
         Climate.Parameter tempDefault2 = Climate.Parameter.span(-0.8F, -0.4F);
-        Climate.Parameter tempDefault3 = Climate.Parameter.span(0.0F, 0.4F);
+        Climate.Parameter tempDefault3 = Climate.Parameter.span(0.0F, 0.35F);
         Climate.Parameter tempDefault4 = Climate.Parameter.span(0.4F, 0.93F);
         Climate.Parameter tempDefault5 = Climate.Parameter.span(0.93F, 0.94F);
         Climate.Parameter tempDefault6 = Climate.Parameter.span(0.94F, 1.0F);
@@ -47,7 +47,7 @@ public class AncientAetherRegion extends Region {
         this.addBiome(mapper, new Climate.ParameterPoint(tempDefault3, Climate.Parameter.span(0.0F, 0.8F), fullRange, fullRange, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_MEADOW);
         this.addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(-0.1F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                AetherBiomes.SKYROOT_MEADOW);
+                AncientAetherBiomes.QUICKSOIL_DESERT);
         this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(-0.3F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_MEADOW);
         this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.8F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
