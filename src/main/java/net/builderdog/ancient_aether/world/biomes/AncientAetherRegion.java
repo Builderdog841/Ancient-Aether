9e50bb9682cc59ee8c66e6131eceabd7da131d88
Builderdog841 <timon.kempter@gmail.com>
2023-08-07 15:47:23 +0200

    Progress on the Biomes

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
@@ -25,24 +25,37 @@ public class AncientAetherRegion extends Region {
         Climate.Parameter fullRange = Climate.Parameter.span(-1.0F, 1.0F);
 
         //SAKURA JUNGLE
-        Climate.Parameter temps1 = Climate.Parameter.span(0.5F, 0.94F);
+        Climate.Parameter temps1 = Climate.Parameter.span(0.6F, 0.94F);
+
+        //SPARSE_SAKURA JUNGLE
+        Climate.Parameter temps2 = Climate.Parameter.span(0.5F, 0.6F);
+        Climate.Parameter temps3 = Climate.Parameter.span(0.94F, 1F);
 
         //QUICKSOIL DESERT
-        Climate.Parameter temps2 = Climate.Parameter.span(-0.8F, -0.3F);
+        Climate.Parameter temps4 = Climate.Parameter.span(-0.8F, -0.3F);
+
+        //GILDED QUICKSOIL DESERT
+        Climate.Parameter temps5 = Climate.Parameter.span(-0.8F, -0.4F);
 
         //HIGHLANDS TAIGA
-        Climate.Parameter temps3 = Climate.Parameter.span(0.2F, 0.5F);
+        Climate.Parameter temps6 = Climate.Parameter.span(0.2F, 0.5F);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(temps1, Climate.Parameter.span(0F, 0.1F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(temps1, Climate.Parameter.span(0F, 1F), fullRange, fullRange, fullRange, fullRange, 0),
                 AncientAetherBiomes.SAKURA_JUNGLE);
 
         this.addBiome(mapper, new Climate.ParameterPoint(temps2, Climate.Parameter.span(-1F, 0F), fullRange, fullRange, fullRange, fullRange, 0),
+                AncientAetherBiomes.SPARSE_SAKURA_JUNGLE);
+
+        this.addBiome(mapper, new Climate.ParameterPoint(temps3, Climate.Parameter.span(-1F, 0F), fullRange, fullRange, fullRange, fullRange, 0),
+                AncientAetherBiomes.SPARSE_SAKURA_JUNGLE);
+
+        this.addBiome(mapper, new Climate.ParameterPoint(temps4, Climate.Parameter.span(-1F, 0F), fullRange, fullRange, fullRange, fullRange, 0),
                 AncientAetherBiomes.QUICKSOIL_DESERT);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(temps2, Climate.Parameter.span(0F, 1F), fullRange, fullRange, fullRange, fullRange, 0),
-                AncientAetherBiomes.QUICKSOIL_DESERT);
+        this.addBiome(mapper, new Climate.ParameterPoint(temps5, Climate.Parameter.span(0F, 1F), fullRange, fullRange, fullRange, fullRange, 0),
+                AncientAetherBiomes.GILDED_QUICKSOIL_DESERT);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(temps3, Climate.Parameter.span(0F, 1F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(temps6, Climate.Parameter.span(0F, 1F), fullRange, fullRange, fullRange, fullRange, 0),
                 AncientAetherBiomes.HIGHLAND_TAIGA);
     }
 }
