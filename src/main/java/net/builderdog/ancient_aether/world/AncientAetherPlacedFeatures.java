6918e91bad1edec43d8f8f5b49ded3bc3eccae5c
Builderdog841 <timon.kempter@gmail.com>
2023-08-09 20:14:11 +0200

    Tweaked Skyroot Thorn Bush and Sparse Highland Taiga Trees Spawnrate

--- src/main/java/net/builderdog/ancient_aether/world/AncientAetherPlacedFeatures.java
+++ src/main/java/net/builderdog/ancient_aether/world/AncientAetherPlacedFeatures.java
@@ -73,7 +73,7 @@ public class AncientAetherPlacedFeatures {
                 new DungeonBlacklistFilter());
 
         register(context, SKYROOT_THORN_BUSH_PATCH, configuredFeatures.getOrThrow(AncientAetherConfiguredFeatures.SKYROOT_THORN_BUSH_PATCH),
-                CountPlacement.of(2),
+                CountPlacement.of(5),
                 InSquarePlacement.spread(),
                 PlacementUtils.HEIGHTMAP,
                 BiomeFilter.biome(),
