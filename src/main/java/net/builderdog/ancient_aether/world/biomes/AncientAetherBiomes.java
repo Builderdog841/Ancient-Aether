4fe45574746e9368c7bde1a3ad6dfceb411339f1
Builderdog841 <timon.kempter@gmail.com>
2023-08-17 19:04:32 +0200

    Genesis Compat Fixes and Highsproot Grove Biome

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherBiomes.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherBiomes.java
@@ -13,15 +13,18 @@ public class AncientAetherBiomes {
     public static final ResourceKey<Biome> FESTIVE_WYNDCAP_TAIGA = createKey("festive_wyndcap_taiga");
     public static final ResourceKey<Biome> WYNDCAP_HIGHLAND = createKey("wyndcap_highland");
     public static final ResourceKey<Biome> WYNDCAP_PEAKS = createKey("wyndcap_peaks");
-    public static final ResourceKey<Biome> SKYROOT_PINE_FOREST = createKey("skyroot_pine_forest");
 
     //Cave Biomes
     public static final ResourceKey<Biome> AEROGEL_CAVES = createKey("aerogel_caves");
     public static final ResourceKey<Biome> OVERGROWN_CAVERNS = createKey("overgrown_caverns");
 
+    //Default Biomes
+    public static final ResourceKey<Biome> SKYROOT_PINE_FOREST = createKey("skyroot_pine_forest");
+    public static final ResourceKey<Biome> SKYROOT_JUNGLE = createKey("skyroot_jungle");
+    public static final ResourceKey<Biome> HIGHSPROOT_GROVE = createKey("highsproot_grove");
+
     //Sakura Jungle
     public static final ResourceKey<Biome> SAKURA_JUNGLE = createKey("sakura_jungle");
-    public static final ResourceKey<Biome> SKYROOT_JUNGLE = createKey("skyroot_jungle");
 
     private static ResourceKey<Biome> createKey(String name) {
         return ResourceKey.create(Registries.BIOME, new ResourceLocation(AncientAether.MOD_ID, name));
