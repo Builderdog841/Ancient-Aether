2c03bedbdff686a894eac1913575952f512ab4ce
Builderdog841 <timon.kempter@gmail.com>
2023-08-27 15:37:29 +0200

    Biome Spawning Rebalancing

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
@@ -29,15 +29,14 @@ public class AncientAetherRegion extends Region {
         Climate.Parameter tempDefault3 = Climate.Parameter.span(0.93F, 0.94F);
         Climate.Parameter tempDefault4 = Climate.Parameter.span(0.94F, 1.0F);
 
-        Climate.Parameter tempSakura1 = Climate.Parameter.span(-0.7F, -0.25F);
-        Climate.Parameter tempSakura2 = Climate.Parameter.span(-0.325F, -0.2F);
-        Climate.Parameter tempSakura3 = Climate.Parameter.span(-0.65F, -0.325F);
-        Climate.Parameter tempSakura4 = Climate.Parameter.span(-0.8F, -0.65F);
+        Climate.Parameter tempSakura1 = Climate.Parameter.span(-0.9F, -0.3F);
+        Climate.Parameter tempSakura2 = Climate.Parameter.span(-0.4F, -0.2F);
+        Climate.Parameter tempSakura3 = Climate.Parameter.span(-0.8F, -0.4F);
+        Climate.Parameter tempSakura4 = Climate.Parameter.span(-1F, -0.8F);
 
-        Climate.Parameter tempWyndcaps1 = Climate.Parameter.span(0.35F, 0.4F);
-        Climate.Parameter tempWyndcaps2 = Climate.Parameter.span(0.4F, 0.8F);
-        Climate.Parameter tempWyndcaps3 = Climate.Parameter.span(0.8F, 0.93F);
-        Climate.Parameter tempWyndcaps4 = Climate.Parameter.span(0.35F, 0.93F);
+        Climate.Parameter tempWyndcaps1 = Climate.Parameter.span(0.35F, 0.8F);
+        Climate.Parameter tempWyndcaps2 = Climate.Parameter.span(0.8F, 0.93F);
+        Climate.Parameter tempWyndcaps3 = Climate.Parameter.span(0.35F, 0.93F);
 
         addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(0.5F, 0.8F), fullRange, fullRange, Climate.Parameter.span(-1.0F, 0.3F), fullRange, 0),
                 AetherBiomes.SKYROOT_FOREST);
@@ -79,17 +78,14 @@ public class AncientAetherRegion extends Region {
                 AetherBiomes.SKYROOT_WOODLAND);
 
         addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps1, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-0.35F, 1.0F), fullRange, 0),
-                AncientAetherBiomes.SKYROOT_PINE_FOREST);
-        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps2, Climate.Parameter.span(-1.0F, 0.2F), fullRange, fullRange, Climate.Parameter.span(-0.35F, 1.0F), fullRange, 0),
                 AncientAetherBiomes.WYNDCAP_TAIGA);
-        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps2, Climate.Parameter.span(0.2F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-0.35F, 1.0F), fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps2, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-0.35F, 1.0F), fullRange, 0),
                 AncientAetherBiomes.FESTIVE_WYNDCAP_TAIGA);
-        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps4, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-0.6F, -0.35F), fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps3, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-0.6F, -0.35F), fullRange, 0),
                 AncientAetherBiomes.WYNDCAP_HIGHLAND);
-        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps4, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-1.5F, -0.6F), fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps3, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-1.5F, -0.6F), fullRange, 0),
                 AncientAetherBiomes.WYNDCAP_PEAKS);
-        addBiome(mapper, new Climate.ParameterPoint(tempWyndcaps3, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, Climate.Parameter.span(-0.35F, 1.0F), fullRange, 0),
-                AncientAetherBiomes.SKYROOT_PINE_FOREST);
+
         addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(0.0F, 0.3F), fullRange, fullRange, Climate.Parameter.span(-1.0F, 0.3F), fullRange, 0),
                 AncientAetherBiomes.SKYROOT_PINE_FOREST);
 
