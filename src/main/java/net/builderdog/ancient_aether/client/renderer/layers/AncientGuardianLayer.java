d02538dccc51f0d96c6770e64b9b25f56e9be6ea
Builderdog841 <timon.kempter@gmail.com>
2023-08-28 18:21:03 +0200

    Fixed Ancient Guardian Glow Texture

--- src/main/java/net/builderdog/ancient_aether/client/renderer/layers/AncientGuardianLayer.java
+++ src/main/java/net/builderdog/ancient_aether/client/renderer/layers/AncientGuardianLayer.java
@@ -1,7 +1,5 @@
 package net.builderdog.ancient_aether.client.renderer.layers;
 
-import com.aetherteam.aether_genesis.client.renderer.entity.model.SentryGuardianModel;
-import com.aetherteam.aether_genesis.entity.monster.boss.SentryGuardian;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import javax.annotation.Nonnull;
@@ -16,21 +14,21 @@ import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 
 public class AncientGuardianLayer<T extends AncientGuardian, M extends AncientGuardianModel<T>> extends EyesLayer<T, M> {
-    private static final RenderType GUARDIAN_EYE = RenderType.eyes(new ResourceLocation("ancient_aether", "textures/entity/boss/ancient_guardian/ancient_guardian.png"));
-    private static final RenderType GUARDIAN_EYE_LIT = RenderType.eyes(new ResourceLocation("ancient_aether", "textures/entity/boss/ancient_guardian/ancient_guardian.png"));
+    private static final RenderType GUARDIAN_EYE = RenderType.eyes(new ResourceLocation("ancient_aether", "textures/entity/boss/ancient_guardian/ancient_guardian_glow.png"));
+    private static final RenderType GUARDIAN_EYE_LIT = RenderType.eyes(new ResourceLocation("ancient_aether", "textures/entity/boss/ancient_guardian/ancient_guardian_glow.png"));
 
     public AncientGuardianLayer(RenderLayerParent<T, M> entityRenderer) {
         super(entityRenderer);
     }
 
-    public void render(@Nonnull PoseStack poseStack, MultiBufferSource buffer, int packedLight, @Nonnull SentryGuardian guardian, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        VertexConsumer consumer = buffer.getBuffer(this.renderType(guardian));
-        ((AncientGuardianModel)this.getParentModel()).renderToBuffer(poseStack, consumer, 15728640, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+    public void render(@Nonnull PoseStack poseStack, MultiBufferSource buffer, int packedLight, @Nonnull AncientGuardian guardian, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        VertexConsumer consumer = buffer.getBuffer(renderType(guardian));
+        ((AncientGuardianModel)getParentModel()).renderToBuffer(poseStack, consumer, 15728640, OverlayTexture.NO_OVERLAY, 0.65F, 0.65F, 0.65F, 0.1F);
     }
 
     @Nonnull
-    public RenderType renderType(SentryGuardian guardian) {
-        return guardian.isAwake() ? GUARDIAN_EYE_LIT : this.renderType();
+    public RenderType renderType(AncientGuardian guardian) {
+        return guardian.isAwake() ? GUARDIAN_EYE_LIT : renderType();
     }
 
     @Nonnull
