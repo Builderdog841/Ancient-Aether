ddd5ee2fb900ff62108413e974d8a3e4dad888c2
Builderdog841 <timon.kempter@gmail.com>
2023-09-27 18:13:55 +0200

    Progress on some behind the scenes stuff

--- src/main/java/net/builderdog/ancient_aether/mixin/mixins/client/AdvancementToastMixin.java
+++ src/main/java/net/builderdog/ancient_aether/mixin/mixins/client/AdvancementToastMixin.java
@@ -1,6 +1,7 @@
-package net.builderdog.ancient_aether.mixin.client;
+package net.builderdog.ancient_aether.mixin.mixins.client;
 
 import com.aetherteam.aether.Aether;
+import com.aetherteam.aether.client.AetherSoundEvents;
 import net.builderdog.ancient_aether.client.AncientAetherSoundEvents;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.client.gui.GuiGraphics;
@@ -9,6 +10,7 @@ import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -30,12 +32,13 @@ public class AdvancementToastMixin {
     @Inject(at = @At(value = "FIELD", target = "net/minecraft/client/gui/components/toasts/AdvancementToast.playedSound:Z"), method = "render(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/gui/components/toasts/ToastComponent;J)Lnet/minecraft/client/gui/components/toasts/Toast$Visibility;")
     private void render(GuiGraphics guiGraphics, ToastComponent toastComponent, long timeSinceLastVisible, CallbackInfoReturnable<Toast.Visibility> cir) {
         if (!playedSound) {
-            if (checkRoot()) {
-                playedSound = true;
-                if (advancement.getId().getPath().equals("gold_dungeon")) {
-                    toastComponent.getMinecraft().getSoundManager().play(SimpleSoundInstance.forUI(AncientAetherSoundEvents.UI_TOAST_AETHER_GOLD.get(), 1.0F, 1.0F));
+                switch (this.advancement.getId().getPath()) {
+                    case "cracked_slider":
+                        toastComponent.getMinecraft().getSoundManager().play(SimpleSoundInstance.forUI(AncientAetherSoundEvents.UI_TOAST_AETHER_GOLD.get(), 1.0F, 1.0F));
+                        break;
+                    default:
+                        toastComponent.getMinecraft().getSoundManager().play(SimpleSoundInstance.forUI(AetherSoundEvents.UI_TOAST_AETHER_GENERAL.get(), 1.0F, 1.0F));
                 }
-            }
         }
     }
 
