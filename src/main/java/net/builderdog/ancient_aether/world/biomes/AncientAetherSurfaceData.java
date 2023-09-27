06d21a4c566a4cdfee7d761915f7db608aef69d4
Builderdog841 <timon.kempter@gmail.com>
2023-09-27 19:38:56 +0200

    Powder Snow in Wyndcap Peaks

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherSurfaceData.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherSurfaceData.java
@@ -1,6 +1,7 @@
 package net.builderdog.ancient_aether.world.biomes;
 
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.Noises;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 
 public class AncientAetherSurfaceData {
@@ -8,6 +9,12 @@ public class AncientAetherSurfaceData {
     public static SurfaceRules.RuleSource makeRules()
     {
         return SurfaceRules.sequence(
+
+                SurfaceRules.ifTrue(SurfaceRules.isBiome(AncientAetherBiomes.WYNDCAP_PEAKS),
+                        SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR,
+                                SurfaceRules.ifTrue(SurfaceRules.noiseCondition(Noises.POWDER_SNOW, 0.45D,0.58D),
+                                        SurfaceRules.state(Blocks.POWDER_SNOW.defaultBlockState())))
+                ),
 
                 SurfaceRules.ifTrue(SurfaceRules.isBiome(AncientAetherBiomes.WYNDCAP_PEAKS),
                         SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(Blocks.SNOW_BLOCK.defaultBlockState()))
