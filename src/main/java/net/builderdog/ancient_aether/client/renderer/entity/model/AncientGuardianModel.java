5f18e6e9b9403b6b6571890f3b4802d38e6e01c4
Builderdog841 <timon.kempter@gmail.com>
2023-10-02 20:30:07 +0200

    WIP Ancient Guardian redesign

--- src/main/java/net/builderdog/ancient_aether/client/renderer/entity/model/AncientGuardianModel.java
+++ src/main/java/net/builderdog/ancient_aether/client/renderer/entity/model/AncientGuardianModel.java
@@ -21,23 +21,21 @@ public class AncientGuardianModel<T extends AncientGuardian> extends EntityModel
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
 
-        PartDefinition bone = partdefinition.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(0, 0).addBox(-17.0F, -53.0F, -8.0F, 34.0F, 44.0F, 16.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 60).addBox(-16.0F, -28.0F, -7.0F, 32.0F, 24.0F, 14.0F, new CubeDeformation(0.0F))
-                .texOffs(72, 78).addBox(-10.0F, -68.0F, -7.0F, 20.0F, 20.0F, 20.0F, new CubeDeformation(0.0F))
-                .texOffs(92, 42).addBox(-9.0F, -67.0F, -6.0F, 18.0F, 18.0F, 18.0F, new CubeDeformation(0.0F))
-                .texOffs(132, 86).addBox(-10.0F, -48.0F, 13.0F, 20.0F, 8.0F, 0.0F, new CubeDeformation(0.0F))
-                .texOffs(132, 78).addBox(-10.0F, -76.0F, 13.0F, 20.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
+        PartDefinition bone = partdefinition.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(0, 0).addBox(-15.0F, -53.0F, -8.0F, 30.0F, 44.0F, 16.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 60).addBox(-14.0F, -28.0F, -7.0F, 28.0F, 24.0F, 14.0F, new CubeDeformation(0.0F))
+                .texOffs(64, 78).addBox(-10.0F, -68.0F, -7.0F, 20.0F, 20.0F, 20.0F, new CubeDeformation(0.0F))
+                .texOffs(84, 42).addBox(-9.0F, -67.0F, -6.0F, 18.0F, 18.0F, 18.0F, new CubeDeformation(0.0F))
+                .texOffs(124, 78).addBox(-10.0F, -48.0F, 13.0F, 20.0F, 10.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(124, 88).addBox(-10.0F, -76.0F, 13.0F, 20.0F, 8.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 24.0F, 0.0F, 0.0F, 3.1416F, 0.0F));
 
-        PartDefinition cube_r1 = bone.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(84, 0).addBox(-57.0F, -57.0F, 12.0F, 6.0F, 6.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
+        PartDefinition cube_r1 = bone.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(92, 0).addBox(13.0F, -50.0F, 18.0F, 18.0F, 18.0F, 18.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 136).addBox(15.0F, -33.0F, 20.0F, 14.0F, 24.0F, 14.0F, new CubeDeformation(0.0F))
+                .texOffs(122, 136).addBox(-29.0F, -33.0F, 20.0F, 14.0F, 24.0F, 14.0F, new CubeDeformation(0.0F))
+                .texOffs(72, 118).addBox(-30.0F, -49.0F, 19.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F))
+                .texOffs(128, 102).addBox(14.0F, -49.0F, 19.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 100).addBox(-31.0F, -50.0F, 18.0F, 18.0F, 18.0F, 18.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.5236F, 0.0F, 0.0F));
 
-        PartDefinition cube_r2 = bone.addOrReplaceChild("cube_r2", CubeListBuilder.create().texOffs(0, 98).addBox(13.0F, -50.0F, 18.0F, 18.0F, 18.0F, 18.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 134).addBox(15.0F, -33.0F, 20.0F, 14.0F, 24.0F, 14.0F, new CubeDeformation(0.0F))
-                .texOffs(56, 150).addBox(-29.0F, -33.0F, 20.0F, 14.0F, 24.0F, 14.0F, new CubeDeformation(0.0F))
-                .texOffs(56, 118).addBox(-30.0F, -49.0F, 19.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F))
-                .texOffs(120, 118).addBox(14.0F, -49.0F, 19.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F))
-                .texOffs(100, 0).addBox(-31.0F, -50.0F, 18.0F, 18.0F, 18.0F, 18.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, 0.5236F, 0.0F, 0.0F));
-
-        return LayerDefinition.create(meshdefinition, 192, 192);
+        return LayerDefinition.create(meshdefinition, 256, 256);
     }
 
     @Override
