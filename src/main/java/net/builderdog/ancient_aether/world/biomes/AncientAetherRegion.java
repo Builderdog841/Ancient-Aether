a3cba3ee66f1b619296258e53ffb03e98c9b04b0
Builderdog841 <timon.kempter@gmail.com>
2023-10-06 18:10:28 +0200

    Worlgen and Biome Gen changes

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
@@ -24,7 +24,20 @@ public class AncientAetherRegion extends Region {
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper) {
         Climate.Parameter fullRange = Climate.Parameter.span(-1.5F, 1.5F);
 
-        addBiome(mapper, new Climate.ParameterPoint(fullRange, Climate.Parameter.span(0.5F, 0.8F), fullRange, fullRange, Climate.Parameter.span(-1.0F, 0.3F), fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(Climate.Parameter.span(-0.25F, 0.25F), Climate.Parameter.span(-1F, -0.6F), fullRange, fullRange, fullRange, fullRange, 0),
+                AetherBiomes.SKYROOT_MEADOW);
+        addBiome(mapper, new Climate.ParameterPoint(Climate.Parameter.span(-0.25F, 0.25F), Climate.Parameter.span(-0.6F, 0.4F), fullRange, fullRange, fullRange, fullRange, 0),
+                AetherBiomes.SKYROOT_GROVE);
+        addBiome(mapper, new Climate.ParameterPoint(Climate.Parameter.span(-0.25F, 0.25F), Climate.Parameter.span(0.4F, 1F), fullRange, fullRange, fullRange, fullRange, 0),
+                AetherBiomes.SKYROOT_WOODLAND);
+
+        addBiome(mapper, new Climate.ParameterPoint(Climate.Parameter.span(-0.4F, -0.25F), Climate.Parameter.span(-1F, 0F), fullRange, fullRange, fullRange, fullRange, 0),
+                AetherBiomes.SKYROOT_FOREST);
+        addBiome(mapper, new Climate.ParameterPoint(Climate.Parameter.span(-0.4F, -0.25F), Climate.Parameter.span(-0.3F, 0F), fullRange, fullRange, fullRange, fullRange, 0),
+                AncientAetherBiomes.SKYROOT_PINE_FOREST);
+        addBiome(mapper, new Climate.ParameterPoint(Climate.Parameter.span(-0.4F, -0.25F), Climate.Parameter.span(-0.55F, -0.3F), fullRange, fullRange, fullRange, fullRange, 0),
+                AetherBiomes.SKYROOT_MEADOW);
+        addBiome(mapper, new Climate.ParameterPoint(Climate.Parameter.span(-0.4F, -0.25F), Climate.Parameter.span(-1F, -0.55F), fullRange, fullRange, fullRange, fullRange, 0),
                 AncientAetherBiomes.SKYROOT_PINE_FOREST);
 
     }
