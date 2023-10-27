c9afc095cbf07316a223b2f66bebfae691f3f192
Builderdog841 <timon.kempter@gmail.com>
2023-10-27 20:03:45 +0200

    Made Wind Blowers wearable

--- src/main/java/net/builderdog/ancient_aether/block/functional/WindBlowerBlock.java
+++ src/main/java/net/builderdog/ancient_aether/block/functional/WindBlowerBlock.java
@@ -1,6 +1,8 @@
 package net.builderdog.ancient_aether.block.functional;
 
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Equipable;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
@@ -8,26 +10,34 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.NotNull;
 
-public class WindBlowerBlock extends Block {
+public class WindBlowerBlock extends CarvedPumpkinBlock implements Equipable {
 
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public WindBlowerBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
+
     public BlockState getStateForPlacement(BlockPlaceContext p_48689_) {
         return this.defaultBlockState().setValue(FACING, p_48689_.getHorizontalDirection().getOpposite());
     }
+
     public @NotNull RenderShape getRenderShape(@NotNull BlockState p_48727_) {
         return RenderShape.MODEL;
     }
     public @NotNull BlockState rotate(BlockState blockState, Rotation rotation) {
         return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
     }
+
     public @NotNull BlockState mirror(BlockState blockState, Mirror mirror) {
         return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
     }
+
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_48725_) {
         p_48725_.add(FACING);
+    }
+
+    public EquipmentSlot getEquipmentSlot() {
+        return EquipmentSlot.HEAD;
     }
 }
