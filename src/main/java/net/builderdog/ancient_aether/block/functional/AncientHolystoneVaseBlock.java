f9fed425eb16e56bf8d1749101e19e4270b7244b
Builderdog841 <timon.kempter@gmail.com>
2023-11-02 11:42:26 +0100

    Ancient Holystone Vases now use Loottables instead of Tags

--- src/main/java/net/builderdog/ancient_aether/block/functional/AncientHolystoneVaseBlock.java
+++ src/main/java/net/builderdog/ancient_aether/block/functional/AncientHolystoneVaseBlock.java
@@ -1,26 +1,29 @@
 package net.builderdog.ancient_aether.block.functional;
 
 import com.aetherteam.aether.item.AetherItems;
-import net.builderdog.ancient_aether.AncientAetherTags;
+import com.aetherteam.aether.loot.AetherLoot;
 import net.builderdog.ancient_aether.block.AncientAetherBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.BlockHitResult;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class AncientHolystoneVaseBlock extends HolystoneVaseBlock {
 
@@ -31,9 +34,6 @@ public class AncientHolystoneVaseBlock extends HolystoneVaseBlock {
     @Override
     public @NotNull InteractionResult use(@NotNull BlockState blockstate, @NotNull Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hit) {
         super.use(blockstate, level, pos, player, hand, hit);
-        int x = pos.getX();
-        int y = pos.getY();
-        int z = pos.getZ();
 
         if ((player instanceof ServerPlayer serverPlayer ? serverPlayer.getMainHandItem() : ItemStack.EMPTY).getItem() == AetherItems.AMBROSIUM_SHARD.get()) {
             level.setBlockAndUpdate(pos, AncientAetherBlocks.HOLYSTONE_VASE.get().defaultBlockState().setValue(FACING, blockstate.getValue(FACING)));
@@ -44,12 +44,40 @@ public class AncientHolystoneVaseBlock extends HolystoneVaseBlock {
                 player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
                 stack.shrink(1);
             }
-                if (level instanceof ServerLevel serverLevel) {
-                        ItemEntity entityToSpawn = new ItemEntity(serverLevel, x, y + 1, z, new ItemStack((Objects.requireNonNull(ForgeRegistries.ITEMS.tags()).getTag(AncientAetherTags.Items.ANCIENT_HOLYSTONE_VASE_LOOT).getRandomElement(RandomSource.create()).orElse(Items.AIR))));
-                        entityToSpawn.setPickUpDelay(10);
-                        serverLevel.addFreshEntity(entityToSpawn);
+
+            if (level instanceof ServerLevel serverLevel) {
+
+                LootParams parameters = new LootParams.Builder(serverLevel).withParameter(LootContextParams.BLOCK_STATE, blockstate).withParameter(LootContextParams.TOOL, player.getMainHandItem()).withParameter(LootContextParams.ORIGIN, position()).withParameter(LootContextParams.THIS_ENTITY, player).create(LootContextParamSets.BLOCK);
+                LootTable lootTable = serverLevel.getServer().getLootData().getLootTable(AetherLoot.WHIRLWIND_JUNK);
+                List<ItemStack> list = lootTable.getRandomItems(parameters);
+                for (ItemStack itemstack : list) {
+                    this.spawnAtLocation(itemstack, pos, level);
                 }
+            }
         }
         return InteractionResult.SUCCESS;
+    }
+
+    @Nullable
+    public ItemEntity spawnAtLocation(ItemStack itemStack, BlockPos pos, Level level) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        if (itemStack.isEmpty()) {
+            return null;
+        } else if (level.isClientSide) {
+            return null;
+        } else {
+            ItemEntity itementity = new ItemEntity(level, x, y + 0.5, z, itemStack);
+            itementity.setDefaultPickUpDelay();
+            level.addFreshEntity(itementity);
+            return itementity;
+        }
+    }
+
+    private Vec3 position;
+
+    public Vec3 position() {
+        return position;
     }
 }
