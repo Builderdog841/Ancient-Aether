2b2624775fd948c71248a3759886a18e31a0cced
Builderdog841 <timon.kempter@gmail.com>
2023-11-08 18:00:54 +0100

    Moved Islands closer together

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
@@ -24,17 +24,17 @@ public class AncientAetherRegion extends Region {
     public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper) {
         Climate.Parameter fullRange = Climate.Parameter.span(-1.5F, 1.5F);
 
-        addBiome(mapper, new Climate.ParameterPoint(fullRange, Climate.Parameter.span(-1.5F, -0.3F), Climate.Parameter.span(0.4F, 1.5F), fullRange, fullRange, fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(fullRange, Climate.Parameter.span(-1.5F, -0.2F), Climate.Parameter.span(0.37F, 1.5F), fullRange, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_MEADOW);
-        addBiome(mapper, new Climate.ParameterPoint(fullRange, Climate.Parameter.span(-0.3F, 0.3F), Climate.Parameter.span(0.4F, 1.5F), fullRange, fullRange, fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(fullRange, Climate.Parameter.span(-0.2F, 0.2F), Climate.Parameter.span(0.37F, 1.5F), fullRange, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_GROVE);
-        addBiome(mapper, new Climate.ParameterPoint(fullRange, Climate.Parameter.span(0.3F, 1.5F), Climate.Parameter.span(0.4F, 1.5F), fullRange, fullRange, fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(fullRange, Climate.Parameter.span(0.2F, 1.5F), Climate.Parameter.span(0.37F, 1.5F), fullRange, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_WOODLAND);
 
-        addBiome(mapper, new Climate.ParameterPoint(fullRange, fullRange, Climate.Parameter.span(0.3F, 0.4F), fullRange, fullRange, fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(fullRange, fullRange, Climate.Parameter.span(0.27F, 0.37F), fullRange, fullRange, fullRange, 0),
                 AncientAetherBiomes.QUICKSOIL_COAST);
 
-        addBiome(mapper, new Climate.ParameterPoint(fullRange, fullRange, Climate.Parameter.span(-1.5F, 0.3F), fullRange, fullRange, fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(fullRange, fullRange, Climate.Parameter.span(-1.5F, 0.27F), fullRange, fullRange, fullRange, 0),
                 AncientAetherBiomes.CLOUD_OCEAN);
     }
 }
