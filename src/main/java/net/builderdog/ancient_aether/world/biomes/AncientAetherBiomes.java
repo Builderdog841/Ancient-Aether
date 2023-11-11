20aa5a38ef6127fac29fbb948e2586f8d7a423ca
Builderdog841 <timon.kempter@gmail.com>
2023-11-11 18:26:23 +0100

    Began implementing Holy Coasts and Snowy Wyndcaps

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherBiomes.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherBiomes.java
@@ -12,7 +12,8 @@ public class AncientAetherBiomes extends AetherDensityFunctions {
     public static final ResourceKey<Biome> CLOUD_OCEAN = createKey("cloud_ocean");
     public static final ResourceKey<Biome> QUICKSOIL_COAST = createKey("quicksoil_coast");
     public static final ResourceKey<Biome> DIVINE_COAST = createKey("divine_coast");
-    public static final ResourceKey<Biome> SNOWY_DIVINE_COAST = createKey("snowy_divine_coast");
+    public static final ResourceKey<Biome> HOLY_COAST = createKey("holy_coast");
+    public static final ResourceKey<Biome> SNOWY_HOLY_COAST = createKey("snowy_holy_coast");
     public static final ResourceKey<Biome> WYNDCAP_TAIGA = createKey("wyndcap_taiga");
     public static final ResourceKey<Biome> FESTIVE_WYNDCAP_TAIGA = createKey("festive_wyndcap_taiga");
     public static final ResourceKey<Biome> WYNDCAP_HIGHLAND = createKey("wyndcap_highland");
