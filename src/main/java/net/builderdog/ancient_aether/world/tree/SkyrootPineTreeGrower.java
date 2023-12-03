6053f9f5165d0cfc2d7a51104e78271ac3ee9bbd
Builderdog841 <timon.kempter@gmail.com>
2023-12-03 11:11:14 +0100

    Leaf Code Clean-Up and fixed Sakura Leaves making a regular Leaf Sound

---
+++ src/main/java/net/builderdog/ancient_aether/world/tree/SkyrootPineTreeGrower.java
@@ -0,0 +1,17 @@
+package net.builderdog.ancient_aether.world.tree;
+
+import net.builderdog.ancient_aether.world.AncientAetherConfiguredFeatures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class SkyrootPineTreeGrower extends AbstractTreeGrower {
+    @Nullable
+    @Override
+    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource random, boolean largeHive) {
+        return AncientAetherConfiguredFeatures.SKYROOT_PINE_TREE;
+    }
+}
