13d8495bcb012d77bffc4482c70726820958a541
Builderdog841 <timon.kempter@gmail.com>
2024-01-02 09:52:22 +0100

    Fixed Biome Gen being bugged

--- src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
+++ src/main/java/net/builderdog/ancient_aether/world/biomes/AncientAetherRegion.java
@@ -47,7 +47,7 @@ public class AncientAetherRegion extends Region {
         addBiome(mapper, new Climate.ParameterPoint(tempSkyroot1, Climate.Parameter.span(0.25F, 1.5F), fullRange, erosionDefault, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_MEADOW);
 
-        addBiome(mapper, new Climate.ParameterPoint(tempSkyroot2, Climate.Parameter.span(-0.2F, 0.1F), fullRange, erosionDefault, fullRange, fullRange, 0),
+        addBiome(mapper, new Climate.ParameterPoint(tempSkyroot2, Climate.Parameter.span(-1.5F, -0.2F), fullRange, erosionDefault, fullRange, fullRange, 0),
                 AncientAetherBiomes.SKYROOT_GRASSLAND);
         addBiome(mapper, new Climate.ParameterPoint(tempSkyroot2, Climate.Parameter.span(-0.2F, 0.1F), fullRange, erosionDefault, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_FOREST);
