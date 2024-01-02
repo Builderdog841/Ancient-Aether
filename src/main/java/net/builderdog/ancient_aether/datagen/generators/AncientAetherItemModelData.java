cb518e4a429a678acbdabc0d376e638a2a2931df
Builderdog841 <timon.kempter@gmail.com>
2024-01-02 22:29:08 +0100

    Updated Sakura Moa Egg Texture

--- src/main/java/net/builderdog/ancient_aether/datagen/generators/AncientAetherItemModelData.java
+++ src/main/java/net/builderdog/ancient_aether/datagen/generators/AncientAetherItemModelData.java
@@ -54,7 +54,7 @@ public class AncientAetherItemModelData extends AetherItemModelProvider {
         eggItem(AncientAetherItems.AERONAUTIC_LEAPER_SPAWN_EGG.get());
         eggItem(AncientAetherItems.ANCIENT_GUARDIAN_SPAWN_EGG.get());
         eggItem(AncientAetherItems.ANCIENT_CORE_SPAWN_EGG.get());
-        moaEggItem(AncientAetherItems.SAKURA_MOA_EGG.get(), "");
+        untintedMoaEggItem(AncientAetherItems.SAKURA_MOA_EGG.get());
 
         //Armor Trims
         item(AncientAetherItems.WYND_ARMOR_TRIM_SMITHING_TEMPLATE.get(), "");
@@ -186,17 +186,21 @@ public class AncientAetherItemModelData extends AetherItemModelProvider {
     }
     private void simpleItem(RegistryObject<Item> item) {
         withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(AncientAether.MOD_ID, "item/" + item.getId().getPath()));
+                new ResourceLocation("item/generated"))
+                .texture("layer0", new ResourceLocation(AncientAether.MOD_ID, "item/" + item.getId().getPath()));
     }
-        public void eggItem(Item item) {
-            this.withExistingParent(this.itemName(item), this.mcLoc("item/template_spawn_egg"));
+
+    public void untintedMoaEggItem(Item item) {
+        (withExistingParent(itemName(item), modLoc("item/untinted_moa_egg")))
+                .texture("egg", modLoc( "item/" + itemName(item)))
+                .texture("particle", modLoc( "item/" + itemName(item)));
     }
+
     public void itemLogWallBlock(Block block, Block baseBlock, String location, String modid) {
         ResourceLocation baseTexture = new ResourceLocation(modid, "block/" + location + blockName(baseBlock));
-        (((((withExistingParent(this.blockName(block), this.mcLoc("block/block"))).transforms().transform(ItemDisplayContext.GUI).rotation(30.0F, 135.0F, 0.0F).translation(0.0F, 0.0F, 0.0F).scale(0.625F, 0.625F, 0.625F).end().transform(ItemDisplayContext.FIXED).rotation(0.0F, 90.0F, 0.0F).translation(0.0F, 0.0F, 0.0F).scale(0.5F, 0.5F, 0.5F).end().end()).texture("top", baseTexture + "_top")).texture("side", baseTexture)).element().from(4.0F, 0.0F, 4.0F).to(12.0F, 16.0F, 12.0F).face(Direction.DOWN).uvs(4.0F, 4.0F, 12.0F, 12.0F).texture("#top").cullface(Direction.DOWN).end().face(Direction.UP).uvs(4.0F, 4.0F, 12.0F, 12.0F).texture("#top").end().face(Direction.NORTH).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().face(Direction.SOUTH).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().face(Direction.WEST).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().face(Direction.EAST).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().end()).element().from(5.0F, 0.0F, 0.0F).to(11.0F, 13.0F, 16.0F).face(Direction.DOWN).uvs(5.0F, 0.0F, 11.0F, 16.0F).texture("#top").cullface(Direction.DOWN).end().face(Direction.UP).uvs(5.0F, 0.0F, 11.0F, 16.0F).texture("#top").end().face(Direction.NORTH).uvs(5.0F, 3.0F, 11.0F, 16.0F).texture("#side").cullface(Direction.NORTH).end().face(Direction.SOUTH).uvs(5.0F, 3.0F, 11.0F, 16.0F).texture("#side").cullface(Direction.SOUTH).end().face(Direction.WEST).uvs(0.0F, 3.0F, 16.0F, 16.0F).texture("#side").end().face(Direction.EAST).uvs(0.0F, 3.0F, 16.0F, 16.0F).texture("#side").end().end();
+        (((((withExistingParent(blockName(block), mcLoc("block/block"))).transforms().transform(ItemDisplayContext.GUI).rotation(30.0F, 135.0F, 0.0F).translation(0.0F, 0.0F, 0.0F).scale(0.625F, 0.625F, 0.625F).end().transform(ItemDisplayContext.FIXED).rotation(0.0F, 90.0F, 0.0F).translation(0.0F, 0.0F, 0.0F).scale(0.5F, 0.5F, 0.5F).end().end()).texture("top", baseTexture + "_top")).texture("side", baseTexture)).element().from(4.0F, 0.0F, 4.0F).to(12.0F, 16.0F, 12.0F).face(Direction.DOWN).uvs(4.0F, 4.0F, 12.0F, 12.0F).texture("#top").cullface(Direction.DOWN).end().face(Direction.UP).uvs(4.0F, 4.0F, 12.0F, 12.0F).texture("#top").end().face(Direction.NORTH).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().face(Direction.SOUTH).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().face(Direction.WEST).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().face(Direction.EAST).uvs(4.0F, 0.0F, 12.0F, 16.0F).texture("#side").end().end()).element().from(5.0F, 0.0F, 0.0F).to(11.0F, 13.0F, 16.0F).face(Direction.DOWN).uvs(5.0F, 0.0F, 11.0F, 16.0F).texture("#top").cullface(Direction.DOWN).end().face(Direction.UP).uvs(5.0F, 0.0F, 11.0F, 16.0F).texture("#top").end().face(Direction.NORTH).uvs(5.0F, 3.0F, 11.0F, 16.0F).texture("#side").cullface(Direction.NORTH).end().face(Direction.SOUTH).uvs(5.0F, 3.0F, 11.0F, 16.0F).texture("#side").cullface(Direction.SOUTH).end().face(Direction.WEST).uvs(0.0F, 3.0F, 16.0F, 16.0F).texture("#side").end().face(Direction.EAST).uvs(0.0F, 3.0F, 16.0F, 16.0F).texture("#side").end().end();
     }
     public void itemWoodWallBlock(Block block, Block baseBlock, String location, String modid) {
-        this.wallInventory(this.blockName(block), new ResourceLocation(modid, "block/" + location + this.blockName(baseBlock)));
+        wallInventory(blockName(block), new ResourceLocation(modid, "block/" + location + blockName(baseBlock)));
     }
 }
