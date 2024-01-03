964328185840dcfab095ec0b6cc240ee66f1b964
Builderdog841 <timon.kempter@gmail.com>
2024-01-03 23:08:40 +0100

    Updated Festive Swet Texture and Model

--- src/main/java/net/builderdog/ancient_aether/client/renderer/entity/model/FestiveSwetModel.java
+++ src/main/java/net/builderdog/ancient_aether/client/renderer/entity/model/FestiveSwetModel.java
@@ -27,11 +27,17 @@ public class FestiveSwetModel<T extends Entity> extends HierarchicalModel<T> {
     public static LayerDefinition createInnerBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
-        partdefinition.addOrReplaceChild("inner_layer", CubeListBuilder.create().texOffs(32, 16).addBox(-8.0F, -18.0F, 0.0F, 16.0F, 16.0F, 0.0F, new CubeDeformation(0.0F))
+        partdefinition.addOrReplaceChild("inner_layer", CubeListBuilder.create()
                 .texOffs(0, 16).addBox(-3.0F, -7.0F, -3.0F, 6.0F, 6.0F, 6.0F, new CubeDeformation(0.0F))
                 .texOffs(32, 8).addBox(0.0F, -3.0F, -3.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
                 .texOffs(32, 4).addBox(1.5F, -6.0F, -3.5F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F))
                 .texOffs(32, 0).addBox(-3.5F, -6.0F, -3.5F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+
+        partdefinition.addOrReplaceChild("candy_cane_stem_r1", CubeListBuilder.create()
+                .texOffs(49, 14).addBox(0.0F, -7.0F, -2.0F, 3.0F, 15.0F, 3.0F, new CubeDeformation(0.0F))
+                .texOffs(37, 22).addBox(-7.0F, -7.0F, -2.0F, 3.0F, 7.0F, 3.0F, new CubeDeformation(0.0F))
+                .texOffs(23, 26).addBox(-4.0F, -7.0F, -2.0F, 4.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 16.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
+
         return LayerDefinition.create(meshdefinition, 64, 32);
     }
 
