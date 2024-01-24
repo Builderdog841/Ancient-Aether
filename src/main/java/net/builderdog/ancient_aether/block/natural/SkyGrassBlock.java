508da052ed4aafffba70b557562c3328f8e60f47
Builderdog841 <timon.kempter@gmail.com>
2024-01-24 10:03:44 +0100

    Attempt on Sky Grass growing

--- src/main/java/net/builderdog/ancient_aether/block/natural/SkyGrassBlock.java
+++ src/main/java/net/builderdog/ancient_aether/block/natural/SkyGrassBlock.java
@@ -1,19 +1,28 @@
 package net.builderdog.ancient_aether.block.natural;
 
+import net.builderdog.ancient_aether.block.AncientAetherBlocks;
 import net.builderdog.ancient_aether.block.blockstate.AetherGrassType;
 import net.builderdog.ancient_aether.block.blockstate.AncientAetherBlockStateProperties;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.TallGrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import org.jetbrains.annotations.NotNull;
 
 public class SkyGrassBlock extends TallGrassBlock {
     public static final IntegerProperty LENGTH = AncientAetherBlockStateProperties.LENGTH;
     public static final EnumProperty<AetherGrassType> TYPE = AncientAetherBlockStateProperties.TYPE;
     public SkyGrassBlock(Properties properties) {
         super(properties);
+    }
+
+    public void performBonemeal(@NotNull ServerLevel serverLevel, @NotNull RandomSource randomSource, @NotNull BlockPos blockPos, BlockState blockState) {
+        AncientAetherBlocks.SKY_GRASS.get().defaultBlockState().setValue(LENGTH, blockState.getValue(LENGTH) + 1).setValue(TYPE, blockState.getValue(TYPE));
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> blockBlockStateBuilder) {
