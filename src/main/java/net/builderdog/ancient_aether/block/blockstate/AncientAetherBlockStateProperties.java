de4b80d9bb4c41972faf1792ed2f258ec7c15117
Builderdog841 <timon.kempter@gmail.com>
2024-01-24 09:40:00 +0100

    Turned Length Property into an Integer Property

--- src/main/java/net/builderdog/ancient_aether/block/blockstate/AncientAetherBlockStateProperties.java
+++ src/main/java/net/builderdog/ancient_aether/block/blockstate/AncientAetherBlockStateProperties.java
@@ -1,8 +1,10 @@
 package net.builderdog.ancient_aether.block.blockstate;
 
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
-public class AncientAetherBlockStateProperties {
-    public static final EnumProperty<AetherTallGrassLength> LENGTH = EnumProperty.create("length", AetherTallGrassLength.class);
+public class AncientAetherBlockStateProperties extends BlockStateProperties {
+    public static final IntegerProperty LENGTH = IntegerProperty.create("length", 1, 5);
     public static final EnumProperty<AetherGrassType> TYPE = EnumProperty.create("type", AetherGrassType.class);
 }
--- src/main/java/net/builderdog/ancient_aether/block/natural/SkyGrassBlock.java
+++ src/main/java/net/builderdog/ancient_aether/block/natural/SkyGrassBlock.java
@@ -1,16 +1,16 @@
 package net.builderdog.ancient_aether.block.natural;
 
 import net.builderdog.ancient_aether.block.blockstate.AetherGrassType;
-import net.builderdog.ancient_aether.block.blockstate.AetherTallGrassLength;
 import net.builderdog.ancient_aether.block.blockstate.AncientAetherBlockStateProperties;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.TallGrassBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class SkyGrassBlock extends TallGrassBlock {
-    public static final EnumProperty<AetherTallGrassLength> LENGTH = AncientAetherBlockStateProperties.LENGTH;
+    public static final IntegerProperty LENGTH = AncientAetherBlockStateProperties.LENGTH;
     public static final EnumProperty<AetherGrassType> TYPE = AncientAetherBlockStateProperties.TYPE;
     public SkyGrassBlock(Properties properties) {
         super(properties);
