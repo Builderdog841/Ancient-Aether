e89fcfde811865a9ac3b5b6c9773481e7d36591a
Builderdog841 <timon.kempter@gmail.com>
2024-02-06 23:07:47 +0100

    Updated Highland Buffalo Texture and Model, Tag Fixes

--- src/main/java/net/builderdog/ancient_aether/client/renderer/entity/model/HighlandBuffaloModel.java
+++ src/main/java/net/builderdog/ancient_aether/client/renderer/entity/model/HighlandBuffaloModel.java
@@ -21,36 +21,48 @@ public class HighlandBuffaloModel<T extends HighlandBuffalo> extends EntityModel
     private final ModelPart leftHindLeg;
     private final ModelPart rightHindLeg;
     private final ModelPart head;
-    private final ModelPart highland_buffalo;
+    private final ModelPart buffalo;
 
     public HighlandBuffaloModel(ModelPart root) {
-        highland_buffalo = root.getChild("highland_buffalo");
-        leftFrontLeg = highland_buffalo.getChild("left_front_leg");
-        rightFrontLeg = highland_buffalo.getChild("right_front_leg");
-        leftHindLeg = highland_buffalo.getChild("left_hind_leg");
-        rightHindLeg = highland_buffalo.getChild("right_hind_leg");
-        head = highland_buffalo.getChild("head");
+        buffalo = root.getChild("buffalo");
+        head = buffalo.getChild("head");
+        leftFrontLeg = buffalo.getChild("left_front_leg");
+        rightFrontLeg = buffalo.getChild("right_front_leg");
+        leftHindLeg = buffalo.getChild("left_hind_leg");
+        rightHindLeg = buffalo.getChild("right_hind_leg");
     }
     public static LayerDefinition createBodyLayer() {
         MeshDefinition meshdefinition = new MeshDefinition();
         PartDefinition partdefinition = meshdefinition.getRoot();
-        PartDefinition buffalo = partdefinition.addOrReplaceChild("highland_buffalo", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
-        PartDefinition body = buffalo.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-10.0F, -24.0F, -15.0F, 20.0F, 12.0F, 30.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 18).addBox(10.0F, -12.0F, -15.0F, 0.0F, 6.0F, 30.0F, new CubeDeformation(0.0F))
-                .texOffs(40, 64).addBox(-10.0F, -12.0F, -15.0F, 20.0F, 6.0F, 0.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 62).addBox(-10.0F, -12.0F, 15.0F, 20.0F, 6.0F, 0.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 12).addBox(-10.0F, -12.0F, -15.0F, 0.0F, 6.0F, 30.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-        PartDefinition head = buffalo.addOrReplaceChild("head", CubeListBuilder.create().texOffs(16, 12).addBox(-8.0F, -8.0F, -2.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 54).addBox(-10.0F, -14.0F, 1.0F, 20.0F, 8.0F, 0.0F, new CubeDeformation(0.0F))
-                .texOffs(12, 9).addBox(5.0F, -8.0F, -2.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 0).addBox(-5.0F, -5.0F, -8.0F, 10.0F, 5.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(50, 44).addBox(-5.0F, -10.0F, -4.0F, 10.0F, 10.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -20.0F, -16.0F));
+        PartDefinition buffalo = partdefinition.addOrReplaceChild("buffalo", CubeListBuilder.create(), PartPose.offset(6.0F, 14.0F, 10.0F));
 
-        PartDefinition left_front_leg = buffalo.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(16, 68).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -10.0F, -10.0F));
-        PartDefinition right_front_leg = buffalo.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(70, 0).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, -10.0F, -10.0F));
-        PartDefinition left_hind_leg = buffalo.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(0, 68).addBox(-14.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, -10.0F, 10.0F));
-        PartDefinition right_hind_leg = buffalo.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(0, 9).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, -10.0F, 10.0F));
+        PartDefinition head = buffalo.addOrReplaceChild("head", CubeListBuilder.create().texOffs(46, 77).addBox(-5.0F, -5.0F, -4.0F, 10.0F, 10.0F, 10.0F, new CubeDeformation(0.0F))
+                .texOffs(63, 0).addBox(-10.0F, -10.0F, 1.0F, 20.0F, 10.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(76, 77).addBox(-5.0F, 0.0F, -9.0F, 10.0F, 5.0F, 5.0F, new CubeDeformation(0.0F))
+                .texOffs(8, 34).addBox(5.0F, -3.0F, -2.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 34).addBox(-8.0F, -3.0F, -2.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -13.0F, -26.0F));
+
+        PartDefinition outer_body = buffalo.addOrReplaceChild("outer_body", CubeListBuilder.create().texOffs(63, 53).addBox(-16.0F, -11.0F, -7.0F, 20.0F, 13.0F, 11.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 0).addBox(-17.0F, -13.0F, -25.0F, 22.0F, 15.0F, 19.0F, new CubeDeformation(0.0F))
+                .texOffs(63, 10).addBox(-16.0F, 2.0F, 4.0F, 20.0F, 7.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(57, 46).addBox(-17.0F, 2.0F, -6.0F, 22.0F, 4.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(57, 42).addBox(-17.0F, 2.0F, -25.0F, 22.0F, 4.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(57, 19).addBox(5.0F, 2.0F, -25.0F, 0.0F, 4.0F, 19.0F, new CubeDeformation(0.0F))
+                .texOffs(57, 15).addBox(-17.0F, 2.0F, -25.0F, 0.0F, 4.0F, 19.0F, new CubeDeformation(0.0F))
+                .texOffs(20, 75).addBox(-16.0F, 2.0F, -6.0F, 0.0F, 4.0F, 10.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 75).addBox(4.0F, 2.0F, -6.0F, 0.0F, 4.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        PartDefinition body = buffalo.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 64).addBox(-9.0F, -20.0F, 3.0F, 18.0F, 11.0F, 10.0F, new CubeDeformation(0.0F))
+                .texOffs(0, 34).addBox(-10.0F, -22.0F, -14.0F, 20.0F, 13.0F, 17.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, 10.0F, -10.0F));
+
+        PartDefinition right_hind_leg = buffalo.addOrReplaceChild("right_hind_leg", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        PartDefinition left_hind_leg = buffalo.addOrReplaceChild("left_hind_leg", CubeListBuilder.create().texOffs(0, 0).addBox(-14.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+
+        PartDefinition right_front_leg = buffalo.addOrReplaceChild("right_front_leg", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, -20.0F));
+
+        PartDefinition left_front_leg = buffalo.addOrReplaceChild("left_front_leg", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -4.0F, -2.0F, 4.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-12.0F, 0.0F, -20.0F));
 
         return LayerDefinition.create(meshdefinition, 128, 128);
     }
@@ -67,6 +79,6 @@ public class HighlandBuffaloModel<T extends HighlandBuffalo> extends EntityModel
 
     @Override
     public void renderToBuffer(@Nonnull PoseStack poseStack, @Nonnull VertexConsumer consumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        highland_buffalo.render(poseStack, consumer, packedLight, packedOverlay, red, green, blue, alpha);
+        buffalo.render(poseStack, consumer, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
