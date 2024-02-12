2f6ea5c682da224864cd23c8526f9172c079d9a1
Builderdog841 <timon.kempter@gmail.com>
2024-02-12 22:13:59 +0100

    Coast experiments

---
+++ src/main/java/net/builderdog/ancient_aether/world/configuration/CoastConfiguration.java
@@ -0,0 +1,21 @@
+package net.builderdog.ancient_aether.world.configuration;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.RegistryCodecs;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.util.valueproviders.FloatProvider;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+
+public record CoastConfiguration(BlockStateProvider block, FloatProvider radius, UniformInt yRange, HolderSet<Block> validBlocks) implements FeatureConfiguration {
+    public static final Codec<CoastConfiguration> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
+            BlockStateProvider.CODEC.fieldOf("block").forGetter(CoastConfiguration::block),
+            FloatProvider.CODEC.fieldOf("radius").forGetter(CoastConfiguration::radius),
+            UniformInt.CODEC.fieldOf("y_range").forGetter(CoastConfiguration::yRange),
+            RegistryCodecs.homogeneousList(Registries.BLOCK).fieldOf("valid_blocks").forGetter(CoastConfiguration::validBlocks)
+    ).apply(instance, CoastConfiguration::new));
+}
--- src/main/java/net/builderdog/ancient_aether/world/feature/AncientAetherFeatures.java
+++ src/main/java/net/builderdog/ancient_aether/world/feature/AncientAetherFeatures.java
@@ -1,6 +1,7 @@
 package net.builderdog.ancient_aether.world.feature;
 
 import net.builderdog.ancient_aether.AncientAether;
+import net.builderdog.ancient_aether.world.configuration.CoastConfiguration;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,5 +10,6 @@ import net.minecraftforge.registries.RegistryObject;
 public class AncientAetherFeatures {
     public static final DeferredRegister<Feature<?>> FEATURES = DeferredRegister.create(ForgeRegistries.FEATURES, AncientAether.MOD_ID);
 
+    public static RegistryObject<Feature<CoastConfiguration>> COAST = FEATURES.register("coast", () -> new CoastFeature(CoastConfiguration.CODEC));
     public static RegistryObject<Feature<CloudbedFeature.Config>> CLOUDBED = FEATURES.register("cloudbed", () -> new CloudbedFeature(CloudbedFeature.Config.CODEC));
 }
