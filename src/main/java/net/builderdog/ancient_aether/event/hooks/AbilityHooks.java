2175a35f4dbc2fc17fa384f6388c808e2398e874
Builderdog841 <timon.kempter@gmail.com>
2024-02-15 17:53:43 +0100

    Optimized Log Stripping Code, it should no longer cause crashes

--- src/main/java/net/builderdog/ancient_aether/event/hooks/AbilityHooks.java
+++ src/main/java/net/builderdog/ancient_aether/event/hooks/AbilityHooks.java
@@ -1,13 +1,46 @@
 package net.builderdog.ancient_aether.event.hooks;
 
+import com.google.common.collect.ImmutableMap;
+import net.builderdog.ancient_aether.block.AncientAetherBlocks;
 import net.builderdog.ancient_aether.item.equipment.armor.EquipmentUtil;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+
+import java.util.Map;
 
 public class AbilityHooks {
     public static class ArmorHooks {
 
         public static boolean fallCancellation(LivingEntity entity) {
             return EquipmentUtil.hasFullValkyrumSet(entity);
+        }
+    }
+
+    public static class ToolHooks {
+        public static final Map<Block, Block> STRIPPABLES = (new ImmutableMap.Builder<Block, Block>())
+                .put(AncientAetherBlocks.HIGHSPROOT_LOG.get(), AncientAetherBlocks.STRIPPED_HIGHSPROOT_LOG.get())
+                .put(AncientAetherBlocks.SAKURA_LOG.get(), AncientAetherBlocks.STRIPPED_SAKURA_LOG.get())
+                .put(AncientAetherBlocks.HIGHSPROOT_WOOD.get(), AncientAetherBlocks.STRIPPED_HIGHSPROOT_WOOD.get())
+                .put(AncientAetherBlocks.SAKURA_WOOD.get(), AncientAetherBlocks.STRIPPED_SAKURA_WOOD.get())
+                .put(AncientAetherBlocks.HIGHSPROOT_LOG_WALL.get(), AncientAetherBlocks.STRIPPED_HIGHSPROOT_LOG_WALL.get())
+                .put(AncientAetherBlocks.SAKURA_LOG_WALL.get(), AncientAetherBlocks.STRIPPED_SAKURA_LOG_WALL.get())
+                .put(AncientAetherBlocks.HIGHSPROOT_WOOD_WALL.get(), AncientAetherBlocks.STRIPPED_HIGHSPROOT_WOOD_WALL.get())
+                .put(AncientAetherBlocks.SAKURA_WOOD_WALL.get(), AncientAetherBlocks.STRIPPED_SAKURA_WOOD_WALL.get())
+                .build();
+
+        public static BlockState setupToolActions(LevelAccessor accessor, BlockPos pos, BlockState old, ToolAction action) {
+            Block oldBlock = old.getBlock();
+            if (action == ToolActions.AXE_STRIP) {
+                if (STRIPPABLES.containsKey(oldBlock)) {
+                    return STRIPPABLES.get(oldBlock).withPropertiesOf(old);
+                }
+            }
+            return old;
         }
     }
 }
---
+++ src/main/java/net/builderdog/ancient_aether/event/listeners/ToolAbilityListener.java
@@ -0,0 +1,27 @@
+package net.builderdog.ancient_aether.event.listeners;
+
+import net.builderdog.ancient_aether.AncientAether;
+import net.builderdog.ancient_aether.event.hooks.AbilityHooks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = AncientAether.MOD_ID)
+public class ToolAbilityListener {
+
+    @SubscribeEvent
+    public static void setupToolModifications(BlockEvent.BlockToolModificationEvent event) {
+        LevelAccessor levelAccessor = event.getLevel();
+        BlockPos pos = event.getPos();
+        BlockState oldState = event.getState();
+        ToolAction toolAction = event.getToolAction();
+        BlockState newState = AbilityHooks.ToolHooks.setupToolActions(levelAccessor, pos, oldState, toolAction);
+        if (newState != oldState && !event.isSimulated() && !event.isCanceled()) {
+            event.setFinalState(newState);
+        }
+    }
+}
