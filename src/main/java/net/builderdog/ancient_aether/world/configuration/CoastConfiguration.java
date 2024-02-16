271ad95266cde65fcec3a202077b2d8e822de3ae
Builderdog841 <timon.kempter@gmail.com>
2024-02-16 22:05:44 +0100

    Renamed "radius2" field to "radius_below" in the Coast Feature

--- src/main/java/net/builderdog/ancient_aether/world/configuration/CoastConfiguration.java
+++ src/main/java/net/builderdog/ancient_aether/world/configuration/CoastConfiguration.java
@@ -11,11 +11,11 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
-public record CoastConfiguration(BlockStateProvider block, FloatProvider radius, FloatProvider radius2, UniformInt yRange, HolderSet<Block> validBlocks) implements FeatureConfiguration {
+public record CoastConfiguration(BlockStateProvider block, FloatProvider radius, FloatProvider radius_below, UniformInt yRange, HolderSet<Block> validBlocks) implements FeatureConfiguration {
     public static final Codec<CoastConfiguration> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
             BlockStateProvider.CODEC.fieldOf("block").forGetter(CoastConfiguration::block),
             FloatProvider.CODEC.fieldOf("radius").forGetter(CoastConfiguration::radius),
-            FloatProvider.CODEC.fieldOf("radius2").forGetter(CoastConfiguration::radius2),
+            FloatProvider.CODEC.fieldOf("radius_below").forGetter(CoastConfiguration::radius_below),
             UniformInt.CODEC.fieldOf("y_range").forGetter(CoastConfiguration::yRange),
             RegistryCodecs.homogeneousList(Registries.BLOCK).fieldOf("valid_blocks").forGetter(CoastConfiguration::validBlocks)
     ).apply(instance, CoastConfiguration::new));
