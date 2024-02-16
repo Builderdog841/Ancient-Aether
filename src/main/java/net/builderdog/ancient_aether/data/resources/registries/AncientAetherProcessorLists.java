ae1fd32aa4282e5db9abb266b8ea5f07e063d7dc
Builderdog841 <timon.kempter@gmail.com>
2024-02-16 14:38:14 +0100

    Began adding Processor Lists to Datagen

--- src/main/java/net/builderdog/ancient_aether/data/resources/registries/AncientAetherProcessorLists.java
+++ src/main/java/net/builderdog/ancient_aether/data/resources/registries/AncientAetherProcessorLists.java
@@ -1,13 +1,14 @@
 package net.builderdog.ancient_aether.data.resources.registries;
 
-import net.minecraft.core.HolderGetter;
+import com.google.common.collect.ImmutableList;
+import net.builderdog.ancient_aether.AncientAether;
+import net.builderdog.ancient_aether.block.AncientAetherBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.levelgen.structure.templatesystem.*;
 
 import java.util.List;
 
@@ -26,7 +27,7 @@ public class AncientAetherProcessorLists {
     public static final ResourceKey<StructureProcessorList> ANCIENT_DUNGEON = createKey("ancient_dungeon");
 
     private static ResourceKey<StructureProcessorList> createKey(String name) {
-        return ResourceKey.create(Registries.PROCESSOR_LIST, new ResourceLocation(name));
+        return ResourceKey.create(Registries.PROCESSOR_LIST, new ResourceLocation(AncientAether.MOD_ID, name));
     }
 
     private static void register(BootstapContext<StructureProcessorList> context, ResourceKey<StructureProcessorList> resourceKey, List<StructureProcessor> list) {
@@ -34,6 +35,11 @@ public class AncientAetherProcessorLists {
     }
 
     public static void bootstrap(BootstapContext<StructureProcessorList> context) {
-        HolderGetter<Block> holdergetter = context.lookup(Registries.BLOCK);
+        register(context, ANCIENT_DUNGEON, ImmutableList.of(
+                new RuleProcessor(ImmutableList.of(
+                        new ProcessorRule(new RandomBlockMatchTest(AncientAetherBlocks.LOCKED_AEROGETIC_STONE.get(), 0.05F), AlwaysTrueTest.INSTANCE, AncientAetherBlocks.LIGHT_AEROGETIC_STONE.get().defaultBlockState()),
+                        new ProcessorRule(new RandomBlockMatchTest(AncientAetherBlocks.TREASURE_DOORWAY_AEROGETIC_STONE.get(), 0.05F), AlwaysTrueTest.INSTANCE, AncientAetherBlocks.TREASURE_DOORWAY_AEROGETIC_STONE.get().defaultBlockState()),
+                        new ProcessorRule(new RandomBlockMatchTest(Blocks.VINE, 0.9F), AlwaysTrueTest.INSTANCE, Blocks.AIR.defaultBlockState())
+                ))));
     }
 }
