d3bd3ca53f0314d5dbfa57aff1a01184fb780b91
Builderdog841 <timon.kempter@gmail.com>
2024-02-20 21:13:54 +0100

    Updated Cracked Slider Model

--- src/main/java/net/builderdog/ancient_aether/data/resources/registries/AncientAetherStructures.java
+++ src/main/java/net/builderdog/ancient_aether/data/resources/registries/AncientAetherStructures.java
@@ -7,9 +7,16 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.structure.Structure;
 
 public class AncientAetherStructures {
-    public static final ResourceKey<Structure> ANCIENT_DUNGEON = createKey();
+    public static final ResourceKey<Structure> HOLYSTONE_RUIN = createKey("holystone_ruin");
+    public static final ResourceKey<Structure> MYSTERIOUS_HENGE = createKey("mysterious_henge");
+    public static final ResourceKey<Structure> VALKYRIE_CAMP_SKYROOT = createKey("valkyrie_camp_skyroot");
+    public static final ResourceKey<Structure> VALKYRIE_CAMP_WYNDCAPS = createKey("valkyrie_camp_wyndcaps");
+    public static final ResourceKey<Structure> VALKYRIE_SETTLEMENT_SKYROOT = createKey("valkyrie_settlement_skyroot");
+    public static final ResourceKey<Structure> BRONZE_DUNGEON_WYNDCAPS = createKey("bronze_dungeon_wyndcaps");
+    public static final ResourceKey<Structure> ANCIENT_DUNGEON = createKey("ancient_dungeon");
+    public static final ResourceKey<Structure> SENTRY_LABORATORY = createKey("sentry_laboratory");
 
-    private static ResourceKey<Structure> createKey() {
-        return ResourceKey.create(Registries.STRUCTURE, new ResourceLocation(AncientAether.MOD_ID, "ancient_dungeon"));
+    private static ResourceKey<Structure> createKey(String name) {
+        return ResourceKey.create(Registries.STRUCTURE, new ResourceLocation(AncientAether.MOD_ID, name));
     }
 }
