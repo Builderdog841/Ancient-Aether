4402dbed4fe738f44623f18dbe8cdd4277d15aef
Builderdog841 <timon.kempter@gmail.com>
2024-03-09 12:18:40 +0100

    Fixed Pickaxes being broken

--- src/main/java/net/builderdog/ancient_aether/AncientAether.java
+++ src/main/java/net/builderdog/ancient_aether/AncientAether.java
@@ -130,13 +130,6 @@ public class AncientAether {
             event.addRepositorySource(consumer -> consumer.accept(pack));
         }
 
-        if (ModList.get().isLoaded("deep_aether") && event.getPackType() == PackType.CLIENT_RESOURCES) {
-            var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("packs/deep_aether_asset_compat");
-            var pack = Pack.readMetaAndCreate("builtin/deep_aether_asset_compat", Component.translatable("pack.ancient_aether.deep_aether_asset_compat.title"), true,
-                    path -> new PathPackResources(path, resourcePath, true), PackType.CLIENT_RESOURCES, Pack.Position.TOP, PackSource.BUILT_IN);
-            event.addRepositorySource(consumer -> consumer.accept(pack));
-        }
-
         if (event.getPackType() == PackType.SERVER_DATA) {
             var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("packs/ancient_aether_worldgen_overrides");
             var pack = Pack.readMetaAndCreate("builtin/ancient_aether_worldgen_overrides", Component.translatable("pack.ancient_aether.worldgen_overrides.title"), true,
@@ -161,6 +154,12 @@ public class AncientAether {
                 var pack = Pack.readMetaAndCreate("builtin/deep_aether_compat", Component.translatable("pack.ancient_aether.deep_aether_compat.title"), true,
                         path -> new PathPackResources(path, resourcePath, true), PackType.SERVER_DATA, Pack.Position.TOP, PackSource.SERVER);
 
+                event.addRepositorySource(consumer -> consumer.accept(pack));
+            }
+            if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+                var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("packs/deep_aether_asset_compat");
+                var pack = Pack.readMetaAndCreate("builtin/deep_aether_asset_compat", Component.translatable("pack.ancient_aether.deep_aether_asset_compat.title"), true,
+                        path -> new PathPackResources(path, resourcePath, true), PackType.CLIENT_RESOURCES, Pack.Position.TOP, PackSource.BUILT_IN);
                 event.addRepositorySource(consumer -> consumer.accept(pack));
             }
         }
