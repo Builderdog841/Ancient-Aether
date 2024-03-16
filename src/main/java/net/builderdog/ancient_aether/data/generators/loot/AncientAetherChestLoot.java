058a7c85e354e3d2c30aafd4a0b3097d49c42ed3
Builderdog841 <timon.kempter@gmail.com>
2024-03-16 14:36:39 +0100

    Rebalanced Settlement Loot

--- src/main/java/net/builderdog/ancient_aether/data/generators/loot/AncientAetherChestLoot.java
+++ src/main/java/net/builderdog/ancient_aether/data/generators/loot/AncientAetherChestLoot.java
@@ -1,5 +1,6 @@
 package net.builderdog.ancient_aether.data.generators.loot;
 
+import com.aetherteam.aether.block.AetherBlocks;
 import com.aetherteam.aether.item.AetherItems;
 import net.builderdog.ancient_aether.block.AncientAetherBlocks;
 import net.builderdog.ancient_aether.data.resources.AncientAetherLoot;
@@ -54,7 +55,7 @@ public class AncientAetherChestLoot implements LootTableSubProvider {
                         .add(LootItem.lootTableItem(AetherItems.LEATHER_GLOVES.get()))
                         .add(LootItem.lootTableItem(Items.SADDLE))
                 )
-                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 1.0F))
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 2.0F))
                         .add(LootItem.lootTableItem(AetherItems.BLUE_BERRY.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(4.0F, 8.0F))))
                         .add(LootItem.lootTableItem(AetherItems.ENCHANTED_BERRY.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F))))
                         .add(LootItem.lootTableItem(AncientAetherItems.GRAPES.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 6.0F))))
@@ -82,6 +83,75 @@ public class AncientAetherChestLoot implements LootTableSubProvider {
                 )
                 .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(0.0F, 1.0F))
                         .add(LootItem.lootTableItem(AncientAetherBlocks.FLUFFALO_WOOL.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(4.0F, 8.0F))))
+                )
+        );
+
+        builder.accept(AncientAetherLoot.CHESTS_SETTLEMENT_SKYROOT_GENERIC, LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(3.0F, 5.0F))
+                        .add(LootItem.lootTableItem(AetherItems.BLUE_BERRY.get()).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 6.0F))))
+                        .add(LootItem.lootTableItem(AncientAetherItems.GRAPES.get()).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 6.0F))))
+                        .add(LootItem.lootTableItem(Items.LEATHER).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F))))
+                        .add(LootItem.lootTableItem(Items.SUGAR).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.AECHOR_PETAL.get()).setWeight(3).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.ENCHANTED_BERRY.get()).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.AMBROSIUM_SHARD.get()).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.GOLDEN_AMBER.get()).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 3.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.SKYROOT_BUCKET.get()).setWeight(2).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 1.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.SKYROOT_MILK_BUCKET.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 1.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_GEMSTONE.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.BLUE_MOA_EGG.get())).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 1.0F)))
+                )
+        );
+
+        builder.accept(AncientAetherLoot.CHESTS_SETTLEMENT_SKYROOT_SHOP, LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 1.0F))
+                        .add(LootTableReference.lootTableReference(AncientAetherLoot.CHESTS_SETTLEMENT_SKYROOT_GENERIC))
+                )
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(2.0F, 3.0F))
+                        .add(LootItem.lootTableItem(Items.LEATHER_HELMET))
+                        .add(LootItem.lootTableItem(Items.LEATHER_CHESTPLATE))
+                        .add(LootItem.lootTableItem(Items.LEATHER_LEGGINGS))
+                        .add(LootItem.lootTableItem(Items.LEATHER_BOOTS))
+                        .add(LootItem.lootTableItem(AetherItems.LEATHER_GLOVES.get()))
+                        .add(LootItem.lootTableItem(Items.SADDLE))
+                )
+        );
+
+        builder.accept(AncientAetherLoot.CHESTS_SETTLEMENT_SKYROOT_LIBRARY, LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 1.0F))
+                        .add(LootTableReference.lootTableReference(AncientAetherLoot.CHESTS_SETTLEMENT_SKYROOT_GENERIC))
+                )
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 2.0F))
+                        .add(LootItem.lootTableItem(Items.PAPER).apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 6.0F))))
+                        .add(LootItem.lootTableItem(Items.BOOK).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F))))
+                        .add(LootItem.lootTableItem(AetherBlocks.SKYROOT_BOOKSHELF.get()))
+                )
+        );
+
+        builder.accept(AncientAetherLoot.CHESTS_SETTLEMENT_SECRET, LootTable.lootTable()
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 1.0F))
+                        .add(LootItem.lootTableItem(AetherItems.MUSIC_DISC_CHINCHILLA.get()))
+                        .add(LootItem.lootTableItem(AetherItems.MUSIC_DISC_HIGH.get()))
+                )
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 1.0F))
+                        .add(LootItem.lootTableItem(AetherItems.ENCHANTED_BERRY.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 6.0F))))
+                )
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(2.0F, 2.0F))
+                        .add(LootItem.lootTableItem(Items.PAPER).apply(SetItemCountFunction.setCount(UniformGenerator.between(4.0F, 8.0F))))
+                        .add(LootItem.lootTableItem(Items.BOOK).apply(SetItemCountFunction.setCount(UniformGenerator.between(2.0F, 4.0F))))
+                )
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(1.0F, 2.0F))
+                        .add(LootItem.lootTableItem(AetherItems.AMBROSIUM_SHARD.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 6.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_GEMSTONE.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 3.0F))))
+                )
+                .withPool(LootPool.lootPool().setRolls(UniformGenerator.between(0.0F, 1.0F))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_SWORD.get()).setWeight(2))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_SHOVEL.get()).setWeight(2))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_PICKAXE.get()).setWeight(2))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_AXE.get()).setWeight(2))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_HOE.get()).setWeight(2))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_RING.get()).apply(SetItemCountFunction.setCount(UniformGenerator.between(1.0F, 2.0F))))
+                        .add(LootItem.lootTableItem(AetherItems.ZANITE_PENDANT.get()))
                 )
         );
     }
