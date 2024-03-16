c878b8579ef9bf90210c5d0174a0c39b443fc63d
Builderdog841 <timon.kempter@gmail.com>
2024-03-16 11:11:06 +0100

    Trapped Blocks now have a Tooltip for which mob they summon, Boss Doorway Blocks now display which Boss closes them

--- src/main/java/net/builderdog/ancient_aether/event/hooks/ItemHooks.java
+++ src/main/java/net/builderdog/ancient_aether/event/hooks/ItemHooks.java
@@ -1,9 +1,12 @@
 package net.builderdog.ancient_aether.event.hooks;
 
+import com.aetherteam.aether.block.AetherBlocks;
 import com.aetherteam.aether.item.AetherItems;
+import com.legacy.lost_aether.registry.LCBlocks;
 import com.legacy.lost_aether.registry.LCItems;
 import net.builderdog.ancient_aether.AncientAetherConfig;
 import net.builderdog.ancient_aether.AncientAetherTags;
+import net.builderdog.ancient_aether.block.AncientAetherBlocks;
 import net.builderdog.ancient_aether.item.AncientAetherItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -82,6 +85,7 @@ public class ItemHooks {
                 components.add(position, Component.literal("4 " + "Mid-Air Jumps").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
                 components.add(position + 1, Component.literal("Very Fast " + "Speed").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
             }
+
             if (ModList.get().isLoaded("lost_aether_content")) {
                 if (stack.is(LCItems.orange_moa_egg)) {
                     components.add(position, Component.literal("2 " + "Mid-Air Jumps").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
@@ -91,6 +95,44 @@ public class ItemHooks {
                     components.add(position, Component.literal("3 " + "Mid-Air Jumps").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
                     components.add(position + 1, Component.literal("Very Fast " + "Speed").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
                 }
+            }
+        }
+
+        if (stack.is(AetherBlocks.TRAPPED_CARVED_STONE.get().asItem()) || stack.is(AetherBlocks.TRAPPED_SENTRY_STONE.get().asItem()) || stack.is(AncientAetherBlocks.TRAPPED_CARVED_TILES.get().asItem())) {
+            components.add(position, Component.translatable("entity.aether.sentry").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+        if (stack.is(AetherBlocks.TRAPPED_ANGELIC_STONE.get().asItem()) || stack.is(AetherBlocks.TRAPPED_LIGHT_ANGELIC_STONE.get().asItem())) {
+            components.add(position, Component.translatable("entity.aether.valkyrie").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+        if (stack.is(AetherBlocks.TRAPPED_HELLFIRE_STONE.get().asItem()) || stack.is(AetherBlocks.TRAPPED_LIGHT_HELLFIRE_STONE.get().asItem())) {
+            components.add(position, Component.translatable("entity.aether.fire_minion").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+        if (stack.is(AncientAetherBlocks.TRAPPED_AEROGETIC_STONE.get().asItem()) || stack.is(AncientAetherBlocks.TRAPPED_LIGHT_AEROGETIC_STONE.get().asItem()) || stack.is(AncientAetherBlocks.TRAPPED_CORRUPTED_AEROGETIC_STONE.get().asItem())) {
+            components.add(position, Component.translatable("entity.ancient_aether.aeronautic_leaper").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+
+        if (stack.is(AetherBlocks.BOSS_DOORWAY_CARVED_STONE.get().asItem()) || stack.is(AetherBlocks.BOSS_DOORWAY_SENTRY_STONE.get().asItem())) {
+            components.add(position, Component.translatable("entity.aether.slider").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+        if (stack.is(AetherBlocks.BOSS_DOORWAY_ANGELIC_STONE.get().asItem()) || stack.is(AetherBlocks.BOSS_DOORWAY_LIGHT_ANGELIC_STONE.get().asItem())) {
+            components.add(position, Component.translatable("entity.aether.valkyrie_queen").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+        if (stack.is(AetherBlocks.BOSS_DOORWAY_HELLFIRE_STONE.get().asItem()) || stack.is(AetherBlocks.BOSS_DOORWAY_LIGHT_HELLFIRE_STONE.get().asItem())) {
+            components.add(position, Component.translatable("entity.aether.sun_spirit").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+        if (stack.is(AncientAetherBlocks.BOSS_DOORWAY_WYND_SENTRY_STONE.get().asItem())) {
+            components.add(position, Component.translatable("entity.ancient_aether.mutated_aechor_plant").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+        if (stack.is(AncientAetherBlocks.BOSS_DOORWAY_AEROGETIC_STONE.get().asItem()) || stack.is(AncientAetherBlocks.BOSS_DOORWAY_LIGHT_AEROGETIC_STONE.get().asItem()) || stack.is(AncientAetherBlocks.BOSS_DOORWAY_CORRUPTED_AEROGETIC_STONE.get().asItem())) {
+            components.add(position, Component.literal("").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+        }
+
+        if (ModList.get().isLoaded("lost_aether_content")) {
+            if (stack.is(LCBlocks.trapped_gale_stone.asItem()) || stack.is(LCBlocks.trapped_light_gale_stone.asItem())) {
+                components.add(position, Component.translatable("entity.aether.whirlwind").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
+            }
+            if (stack.is(LCBlocks.boss_doorway_gale_stone.asItem()) || stack.is(LCBlocks.boss_doorway_light_gale_stone.asItem())) {
+                components.add(position, Component.translatable("entity.lost_aether_content.aerwhale_king").withStyle(Style.EMPTY.withColor(ChatFormatting.GRAY)));
             }
         }
     }
