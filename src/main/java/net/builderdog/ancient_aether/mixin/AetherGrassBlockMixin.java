976b9da32c0f1560a58e7c01711e09648dec969d
Zepalesque <60141811+Zepalesque@users.noreply.github.com>
2024-03-20 18:00:27 -0400

    feat: Mixin stuff

--- src/main/java/net/builderdog/ancient_aether/mixin/AetherGrassBlockMixin.java
+++ src/main/java/net/builderdog/ancient_aether/mixin/AetherGrassBlockMixin.java
@@ -1,6 +1,5 @@
 package net.builderdog.ancient_aether.mixin;
 
-import com.aetherteam.aether.block.AetherBlockStateProperties;
 import com.aetherteam.aether.block.AetherBlocks;
 import com.aetherteam.aether.block.natural.AetherGrassBlock;
 import com.aetherteam.aether.mixin.mixins.common.accessor.SpreadingSnowyDirtBlockAccessor;
@@ -14,121 +13,77 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BonemealableBlock;
-import net.minecraft.world.level.block.GrassBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.BiConsumer;
 
-@Mixin(value = AetherGrassBlock.class, remap = false)
-public class AetherGrassBlockMixin extends GrassBlock {
+@Mixin(value = AetherGrassBlock.class)
+public abstract class AetherGrassBlockMixin extends BlockMixin{
 
-    public AetherGrassBlockMixin(BlockBehaviour.Properties properties) {
-        super(properties);
-        registerDefaultState(defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, false).setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.NORMAL));
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    public void aa$constructor(BlockBehaviour.Properties properties, CallbackInfo ci) {
+        this.registerDefaultState(defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.NORMAL));
     }
 
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder);
-        builder.add(AetherBlockStateProperties.DOUBLE_DROPS);
+    @Inject(method = "createBlockStateDefinition", at = @At(value = "TAIL"))
+    protected void aa$defineState(StateDefinition.Builder<Block, BlockState> builder, CallbackInfo ci) {
         builder.add(AncientAetherBlockStateProperties.TYPE);
     }
 
-    @Override
-    public boolean onTreeGrow(BlockState state, LevelReader level, BiConsumer<BlockPos, BlockState> placeFunction, RandomSource randomSource, BlockPos pos, TreeConfiguration config) {
-        placeFunction.accept(pos, AetherBlocks.AETHER_DIRT.get().defaultBlockState().setValue(AetherBlockStateProperties.DOUBLE_DROPS, state.getValue(AetherBlockStateProperties.DOUBLE_DROPS)));
-        return true;
-    }
 
     @SuppressWarnings("deprecation")
-    @Override
-    public void randomTick(@NotNull BlockState state, @NotNull ServerLevel level, @NotNull BlockPos pos, @NotNull RandomSource random) {
+    @Inject(method = "randomTick", at = @At(value = "HEAD"), cancellable = true)
+    public void aa$tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random, CallbackInfo ci) {
         if (!SpreadingSnowyDirtBlockAccessor.callCanBeGrass(state, level, pos)) {
-            if (!level.isAreaLoaded(pos, 3)) return;
+            if (!level.isAreaLoaded(pos, 3)) {
+                ci.cancel();
+                return;
+            }
             level.setBlockAndUpdate(pos, AetherBlocks.AETHER_DIRT.get().defaultBlockState());
         }
         else {
-            if (!level.isAreaLoaded(pos, 3)) return;
+            if (!level.isAreaLoaded(pos, 3)) ci.cancel();;
             if (level.getMaxLocalRawBrightness(pos.above()) >= 9) {
                 BlockState blockstate = defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, state.getValue(AncientAetherBlockStateProperties.TYPE));
                 for(int i = 0; i < 4; ++i) {
                     BlockPos blockpos = pos.offset(random.nextInt(3) - 1, random.nextInt(5) - 3, random.nextInt(3) - 1);
                     if (level.getBlockState(blockpos).is(AetherBlocks.AETHER_DIRT.get()) && SpreadingSnowyDirtBlockAccessor.callCanPropagate(blockstate, level, blockpos)) {
-                        level.setBlockAndUpdate(blockpos, blockstate.setValue(SNOWY, level.getBlockState(blockpos.above()).is(Blocks.SNOW)));
+                        level.setBlockAndUpdate(blockpos, blockstate.setValue(AetherGrassBlock.SNOWY, level.getBlockState(blockpos.above()).is(Blocks.SNOW)));
                     }
                 }
             }
         }
-    }
-
-    public BlockState getStateForPlacement(BlockPlaceContext context) {
-        Holder<Biome> biome = context.getLevel().getBiome(context.getClickedPos());
-        if (biome.is(AncientAetherTags.Biomes.HAS_FROZEN_AETHER_GRASS)) {
-            return defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.FROZEN);
-        }
-        else if (biome.is(AncientAetherTags.Biomes.HAS_PALE_AETHER_GRASS)) {
-            return defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.PALE);
-        }
-        else if (biome.is(AncientAetherTags.Biomes.HAS_ENCHANTED_AETHER_GRASS)) {
-            return defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.ENCHANTED);
-        }
-        else return defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.NORMAL);
+        ci.cancel();
     }
 
     @Override
-    public void performBonemeal(ServerLevel level, @NotNull RandomSource random, BlockPos pos, @NotNull BlockState state) {
-        BlockPos abovePos = pos.above();
-        Block grass = AetherBlocks.AETHER_GRASS_BLOCK.get();
-        Optional<Holder.Reference<PlacedFeature>> grassFeatureOptional = level.registryAccess().registryOrThrow(Registries.PLACED_FEATURE).getHolder(AncientAetherVegetationPlacements.AETHER_GRASS_BONEMEAL);
-
-        start:
-        for (int i = 0; i < 128; ++i) {
-            BlockPos blockPos = abovePos;
-
-            for (int j = 0; j < i / 16; ++j) {
-                blockPos = blockPos.offset(random.nextInt(3) - 1, (random.nextInt(3) - 1) * random.nextInt(3) / 2, random.nextInt(3) - 1);
-                if (!level.getBlockState(blockPos.below()).is(this) || level.getBlockState(blockPos).isCollisionShapeFullBlock(level, blockPos)) {
-                    continue start;
-                }
-            }
-
-            BlockState blockState = level.getBlockState(blockPos);
-            if (blockState.is(grass) && random.nextInt(10) == 0) {
-                ((BonemealableBlock) grass).performBonemeal(level, random, blockPos, blockState);
-            }
-
-            if (blockState.isAir()) {
-                Holder<PlacedFeature> featureHolder;
-                if (random.nextInt(8) == 0) {
-                    List<ConfiguredFeature<?, ?>> list = level.getBiome(blockPos).value().getGenerationSettings().getFlowerFeatures();
-                    if (list.isEmpty()) {
-                        continue;
-                    }
-                    featureHolder = ((RandomPatchConfiguration) list.get(random.nextInt(list.size())).config()).feature();
-                }
-                else {
-                    if (grassFeatureOptional.isEmpty()) {
-                        continue;
-                    }
-                    featureHolder = grassFeatureOptional.get();
-                }
-                featureHolder.value().place(level, level.getChunkSource().getGenerator(), random, blockPos);
-            }
+    protected void aa$getPlacedState(BlockPlaceContext context, CallbackInfoReturnable<BlockState> cir) {
+        Holder<Biome> biome = context.getLevel().getBiome(context.getClickedPos());
+        if (biome.is(AncientAetherTags.Biomes.HAS_FROZEN_AETHER_GRASS)) {
+            cir.setReturnValue(defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.FROZEN));
         }
+        else if (biome.is(AncientAetherTags.Biomes.HAS_PALE_AETHER_GRASS)) {
+            cir.setReturnValue(defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.PALE));
+        }
+        else if (biome.is(AncientAetherTags.Biomes.HAS_ENCHANTED_AETHER_GRASS)) {
+            cir.setReturnValue(defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.ENCHANTED));
+        }
+        else cir.setReturnValue(defaultBlockState().setValue(AncientAetherBlockStateProperties.TYPE, AetherGrassType.NORMAL));
     }
 }
