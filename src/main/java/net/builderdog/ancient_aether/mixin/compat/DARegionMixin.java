381c420c4fce7d20787c658b58f9991440a0c36b
Builderdog841 <timon.kempter@gmail.com>
2024-03-28 12:20:41 +0100

    Made Sun Forests more rare

--- src/main/java/net/builderdog/ancient_aether/mixin/compat/DARegionMixin.java
+++ src/main/java/net/builderdog/ancient_aether/mixin/compat/DARegionMixin.java
@@ -36,8 +36,8 @@ public class DARegionMixin extends Region {
         Climate.Parameter tempSakura = Climate.Parameter.span(0.5F, 1.5F);
 
         Climate.Parameter erosionDefault = Climate.Parameter.span(0.0F, 0.5F);
-        Climate.Parameter erosionSkyrootForest = Climate.Parameter.span(0.0F, 0.25F);
-        Climate.Parameter erosionSunForest = Climate.Parameter.span(0.25F, 0.5F);
+        Climate.Parameter erosionSkyrootForest = Climate.Parameter.span(0.0F, 0.3F);
+        Climate.Parameter erosionSunForest = Climate.Parameter.span(0.3F, 0.5F);
         Climate.Parameter erosionElevated = Climate.Parameter.span(0.5F, 1.5F);
 
         Climate.Parameter depthSurface = Climate.Parameter.span(-1.5F, 0.55F);
