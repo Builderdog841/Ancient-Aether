ae0a59199f13c54fd1e46044e852a3877a2f8725
Builderdog841 <timon.kempter@gmail.com>
2024-04-07 21:27:03 +0200

    Implemented basic Shield of Remediation Functionality

--- src/main/java/net/builderdog/ancient_aether/item/equipment/accessories/shields/ShieldOfRemediationItem.java
+++ src/main/java/net/builderdog/ancient_aether/item/equipment/accessories/shields/ShieldOfRemediationItem.java
@@ -1,7 +1,11 @@
 package net.builderdog.ancient_aether.item.equipment.accessories.shields;
 
+import com.aetherteam.aether.effect.AetherEffects;
 import com.aetherteam.aether.item.accessories.AccessoryItem;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
 
 public class ShieldOfRemediationItem extends AccessoryItem {
     private static final ResourceLocation SHIELD_OF_REMEDIATION = new ResourceLocation("ancient_aether", "textures/models/accessory/shields/shield_of_remediation_accessory.png");
@@ -17,5 +21,13 @@ public class ShieldOfRemediationItem extends AccessoryItem {
 
     public ResourceLocation getShieldOfRemediationSlimTexture() {
         return SHIELD_OF_REMEDIATION_SLIM;
+    }
+
+    @Override
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        LivingEntity livingEntity = slotContext.entity();
+        if (livingEntity.hasEffect(AetherEffects.INEBRIATION.get())) {
+            livingEntity.removeEffect(AetherEffects.INEBRIATION.get());
+        }
     }
 }
