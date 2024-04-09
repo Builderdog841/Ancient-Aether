92654a7f16ab49a94f6d8adad9e28f0aaa122fa5
Builderdog841 <timon.kempter@gmail.com>
2024-04-09 20:31:09 +0200

    Hermes Accessories Speed Buff no longer depends on your Hunger Level instead Hermes Rings now have a 3% speed buff and Pendants 5%

--- src/main/java/net/builderdog/ancient_aether/item/equipment/accessories/pendants/HermesPendantItem.java
+++ src/main/java/net/builderdog/ancient_aether/item/equipment/accessories/pendants/HermesPendantItem.java
@@ -1,9 +1,18 @@
 package net.builderdog.ancient_aether.item.equipment.accessories.pendants;
 
 import com.aetherteam.aether.item.accessories.pendant.PendantItem;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.builderdog.ancient_aether.AncientAether;
 import net.builderdog.ancient_aether.client.AncientAetherSoundEvents;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
+
+import java.util.UUID;
 
 public class HermesPendantItem extends PendantItem {
 
@@ -11,16 +20,10 @@ public class HermesPendantItem extends PendantItem {
         super(new ResourceLocation(AncientAether.MODID, "hermes_pendant"), AncientAetherSoundEvents.ITEM_ACCESSORY_EQUIP_HERMES_PENDANT, properties);
     }
 
-    /*
     @Override
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext context, UUID uuid, ItemStack stack) {
-        if (context.entity() instanceof Player player) {
-            float foodLevel = player.getFoodData().getFoodLevel();
-            Multimap<Attribute, AttributeModifier> attributes = HashMultimap.create();
-            attributes.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(uuid, "Hermes Pendant Ability", foodLevel * 0.002, AttributeModifier.Operation.ADDITION));
-            return attributes;
-        }
-        return null;
+        Multimap<Attribute, AttributeModifier> attributes = HashMultimap.create();
+        attributes.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(uuid, "Hermes Pendant Ability", 0.05, AttributeModifier.Operation.MULTIPLY_TOTAL));
+        return attributes;
     }
-     */
 }
--- src/main/java/net/builderdog/ancient_aether/item/equipment/accessories/rings/HermesRingItem.java
+++ src/main/java/net/builderdog/ancient_aether/item/equipment/accessories/rings/HermesRingItem.java
@@ -1,7 +1,16 @@
 package net.builderdog.ancient_aether.item.equipment.accessories.rings;
 
 import com.aetherteam.aether.item.accessories.ring.RingItem;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import net.builderdog.ancient_aether.client.AncientAetherSoundEvents;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ItemStack;
+import top.theillusivec4.curios.api.SlotContext;
+
+import java.util.UUID;
 
 public class HermesRingItem extends RingItem {
 
@@ -9,16 +18,10 @@ public class HermesRingItem extends RingItem {
         super(AncientAetherSoundEvents.ITEM_ACCESSORY_EQUIP_HERMES_RING, properties);
     }
 
-    /*
     @Override
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext context, UUID uuid, ItemStack stack) {
-        if (context.entity() instanceof Player player) {
-            float foodLevel = player.getFoodData().getFoodLevel();
             Multimap<Attribute, AttributeModifier> attributes = HashMultimap.create();
-            attributes.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(uuid, "Hermes Ring Ability", foodLevel * 0.001, AttributeModifier.Operation.ADDITION));
+            attributes.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(uuid, "Hermes Ring Ability", 0.03, AttributeModifier.Operation.MULTIPLY_TOTAL));
             return attributes;
-        }
-        return null;
     }
-     */
 }
