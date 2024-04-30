ffef4a3047460871383a2fb55ef749a0154f2bbf
Builderdog841 <timon.kempter@gmail.com>
2024-04-30 21:39:50 +0200

    Divine Protection now buffs your Armor Toughness

--- src/main/java/net/builderdog/ancient_aether/effect/AncientAetherEffects.java
+++ src/main/java/net/builderdog/ancient_aether/effect/AncientAetherEffects.java
@@ -4,6 +4,8 @@ import net.builderdog.ancient_aether.AncientAether;
 import net.builderdog.ancient_aether.effect.mob.DivineProtectionEffect;
 import net.builderdog.ancient_aether.effect.mob.NatureBoostEffect;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -11,6 +13,6 @@ import net.minecraftforge.registries.RegistryObject;
 public class AncientAetherEffects {
     public static final DeferredRegister<MobEffect> EFFECTS = DeferredRegister.create(ForgeRegistries.MOB_EFFECTS, AncientAether.MODID);
 
-    public static final RegistryObject<MobEffect> DIVINE_PROTECTION = EFFECTS.register("divine_protection", DivineProtectionEffect::new);
+    public static final RegistryObject<MobEffect> DIVINE_PROTECTION = EFFECTS.register("divine_protection", () -> new DivineProtectionEffect().addAttributeModifier(Attributes.ARMOR_TOUGHNESS, "CB33E1C-E2D6-4A0B-9562-55C25FE53A1E", 3, AttributeModifier.Operation.ADDITION));
     public static final RegistryObject<MobEffect> NATURE_BOOST = EFFECTS.register("nature_boost", NatureBoostEffect::new);
 }
--- src/main/java/net/builderdog/ancient_aether/effect/mob/DivineProtectionEffect.java
+++ src/main/java/net/builderdog/ancient_aether/effect/mob/DivineProtectionEffect.java
@@ -4,6 +4,7 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 
 public class DivineProtectionEffect extends MobEffect {
+
     public DivineProtectionEffect() {
         super(MobEffectCategory.BENEFICIAL, 0x6A8282);
     }
