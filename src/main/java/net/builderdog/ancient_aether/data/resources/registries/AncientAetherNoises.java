88eed101788636cb2c5666cadbd261f879bf22fe
Builderdog841 <timon.kempter@gmail.com>
2024-05-09 14:06:25 +0200

    Noise Cave Generation Improvements

--- src/main/java/net/builderdog/ancient_aether/data/resources/registries/AncientAetherNoises.java
+++ src/main/java/net/builderdog/ancient_aether/data/resources/registries/AncientAetherNoises.java
@@ -27,7 +27,7 @@ public class AncientAetherNoises {
         register(context, VEGETATION, -8, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0);
         register(context, EROSION, -9, 1.5, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
         register(context, JAGGED, -16, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
-        register(context, NOISE_CAVES, -7, 1.0, 2.5, 1.5, 0.0, 0.5, 0.0, 0.0);
+        register(context, NOISE_CAVES, -6, 1.0, 2.0, 1.0, 0.0, 0.0, 0.0, 0.0);
         register(context, AMPLIFIER, -7, 1.0, 2.0, 1.0, 0.0, 0.0, 0.0, 0.0);
         register(context, AMPLIFIER_FACTOR, -8, 0.8, 0.6, 0.25, 0.0, 0.0, 0.0, 0.0);
         register(context, SKYLANDS_AQUIFER, -6, 1.5, 0.5, 0.0, 0.0, 0.0);
