07159ce938ede76d6b6343c880616d86f8f18e77
Builderdog841 <timon.kempter@gmail.com>
2024-05-11 14:07:15 +0200

    Ancient Vases now spawn Dust Particles when broken

--- src/main/java/net/builderdog/ancient_aether/block/blocktype/AncientVaseBlock.java
+++ src/main/java/net/builderdog/ancient_aether/block/blocktype/AncientVaseBlock.java
@@ -1,6 +1,7 @@
 package net.builderdog.ancient_aether.block.blocktype;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.item.ItemEntity;
@@ -21,6 +22,8 @@ import java.util.List;
 
 public class AncientVaseBlock extends VaseBlock {
     private final ResourceLocation vaseLootTable;
+
+    @SuppressWarnings("unused")
     private Vec3 position;
 
     public AncientVaseBlock(ResourceLocation vaseLootTable, Properties properties) {
@@ -30,14 +33,14 @@ public class AncientVaseBlock extends VaseBlock {
 
     public void playerDestroy(@NotNull Level level, @NotNull Player player, @NotNull BlockPos pos, @NotNull BlockState state, @Nullable BlockEntity blockEntity, @NotNull ItemStack tool) {
         super.playerDestroy(level, player, pos, state, blockEntity, tool);
-            if (level instanceof ServerLevel serverLevel) {
-                LootParams parameters = new LootParams.Builder(serverLevel).withParameter(LootContextParams.BLOCK_STATE, state).withParameter(LootContextParams.TOOL, player.getMainHandItem()).withParameter(LootContextParams.ORIGIN, position).withParameter(LootContextParams.THIS_ENTITY, player).create(LootContextParamSets.BLOCK);
-                LootTable lootTable = serverLevel.getServer().getLootData().getLootTable(vaseLootTable);
-                List<ItemStack> list = lootTable.getRandomItems(parameters);
-                for (ItemStack itemstack : list) {
-                    spawnAtLocation(itemstack, pos, level);
-                }
+        if (level instanceof ServerLevel serverLevel) {
+            LootParams parameters = new LootParams.Builder(serverLevel).withParameter(LootContextParams.BLOCK_STATE, state).withParameter(LootContextParams.TOOL, player.getMainHandItem()).withParameter(LootContextParams.ORIGIN, position).withParameter(LootContextParams.THIS_ENTITY, player).create(LootContextParamSets.BLOCK);
+            LootTable lootTable = serverLevel.getServer().getLootData().getLootTable(vaseLootTable);
+            List<ItemStack> list = lootTable.getRandomItems(parameters);
+            for (ItemStack itemstack : list) {
+                spawnAtLocation(itemstack, pos, level);
             }
+        }
     }
 
     public void spawnAtLocation(ItemStack itemStack, BlockPos pos, Level level) {
@@ -49,6 +52,16 @@ public class AncientVaseBlock extends VaseBlock {
             ItemEntity itementity = new ItemEntity(level, x, y + 0.5, z, itemStack);
             itementity.setDefaultPickUpDelay();
             level.addFreshEntity(itementity);
+            spawnDustParticles(level, pos);
+        }
+    }
+
+    public static void spawnDustParticles(Level level, BlockPos pos) {
+        double x = (double)pos.getX() + (double)(level.getRandom().nextFloat() - level.getRandom().nextFloat()) * 0.125;
+        double y = (double)pos.getY() + (double)(level.getRandom().nextFloat() - level.getRandom().nextFloat()) * 0.125;
+        double z = (double)pos.getZ() + (double)(level.getRandom().nextFloat() - level.getRandom().nextFloat()) * 0.125;
+        if (level instanceof ServerLevel serverLevel) {
+            serverLevel.sendParticles(ParticleTypes.POOF, x, y, z, 3, 0.5, 0.75, 0.5, 0.0);
         }
     }
 }
