5a49b416228e6b7e6f6bfa631212263ba6288f29
Builderdog841 <timon.kempter@gmail.com>
2024-05-17 14:57:23 +0200

    Rewrote Gradle Script

--- src/main/java/net/builderdog/ancient_aether/mixin/BlockMixin.java
+++ src/main/java/net/builderdog/ancient_aether/mixin/BlockMixin.java
@@ -1,13 +1,9 @@
 package net.builderdog.ancient_aether.mixin;
 
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
 public abstract class BlockMixin {
