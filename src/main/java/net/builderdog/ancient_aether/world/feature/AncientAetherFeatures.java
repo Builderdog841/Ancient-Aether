c903c17edaafda224d1de85ecda570ef568efe30
Builderdog841 <timon.kempter@gmail.com>
2024-06-18 19:01:02 +0200

    Began adding new Lake Code

--- src/main/java/net/builderdog/ancient_aether/world/feature/AncientAetherFeatures.java
+++ src/main/java/net/builderdog/ancient_aether/world/feature/AncientAetherFeatures.java
@@ -4,6 +4,7 @@ import net.builderdog.ancient_aether.AncientAether;
 import net.builderdog.ancient_aether.world.feature.configuration.CloudbedConfiguration;
 import net.builderdog.ancient_aether.world.feature.configuration.CoastConfiguration;
 import net.builderdog.ancient_aether.world.feature.configuration.CrystalIslandConfiguration;
+import net.builderdog.ancient_aether.world.feature.configuration.NoiseLakeConfiguration;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
@@ -16,6 +17,7 @@ public class AncientAetherFeatures {
 
     public static DeferredHolder<Feature<?>, Feature<SimpleBlockConfiguration>> SKY_GRASS = FEATURES.register("sky_grass", () -> new SkyGrassFeature(SimpleBlockConfiguration.CODEC));
     public static DeferredHolder<Feature<?>, Feature<CoastConfiguration>> COAST = FEATURES.register("coast", () -> new CoastFeature(CoastConfiguration.CODEC));
+    public static DeferredHolder<Feature<?>, Feature<NoiseLakeConfiguration>> NOISE_LAKE = FEATURES.register("noise_lake", () -> new NoiseLakeFeature(NoiseLakeConfiguration.CODEC));
     public static DeferredHolder<Feature<?>, Feature<CrystalIslandConfiguration>> CONFIGURABLE_CRYSTAL_ISLAND = FEATURES.register("configurable_crystal_island", () -> new ConfigurableCrystalIslandFeature(CrystalIslandConfiguration.CODEC));
     public static DeferredHolder<Feature<?>, Feature<CloudbedConfiguration>> CLOUDBED = FEATURES.register("cloudbed", () -> new CloudbedFeature(CloudbedConfiguration.CODEC));
     public static DeferredHolder<Feature<?>, Feature<NoneFeatureConfiguration>> DUNGEON_ENTRANCE = FEATURES.register("dungeon_entrance", () -> new DungeonEntranceFeature(NoneFeatureConfiguration.CODEC));
